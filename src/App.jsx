daedbb8725d9694452517de7b19acda3ee3f2499
Roman Serediuk <box4rom@gmail.com>
2023-03-17 18:34:42 +0200

    implement routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Homepage from "./pages/Homepage";
+import Publications from "./pages/Publications";
+import Research from "./pages/Research";
 
 const App = () => {
   return (
-    <div className="container">
-      <h1 className="text-center">PhD Palchycov</h1>
-      <Routes></Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Homepage />} />
+        <Route path="publications" element={<Publications />} />
+        <Route path="research" element={<Research />} />
+      </Route>
+    </Routes>
   );
 };
 
