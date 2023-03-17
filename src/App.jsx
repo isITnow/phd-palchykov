bb6d370e0a4630ef45594fbb9d0fdd74c19e5a37
Roman Serediuk <box4rom@gmail.com>
2023-03-17 16:48:21 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1 className="text-center">PhD Palchycov</h1>
+    </div>
+  );
+};
+
+export default App;
