97a7c66b7e749fde89baaf05be8e6f159a35b2c8
Roman Serediuk <box4rom@gmail.com>
2023-03-17 16:56:01 +0200

    add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="container">
       <h1 className="text-center">PhD Palchycov</h1>
+      <Routes></Routes>
     </div>
   );
 };
