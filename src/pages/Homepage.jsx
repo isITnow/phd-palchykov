fd9ef49d628d8bc2ed532a329160d9fb0ec7810b
Roman Serediuk <box4rom@gmail.com>
2023-03-22 20:56:51 +0200

    implement About component, add Vitalii photo

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,24 @@
-import Carousel from "../components/Carousel/Carousel";
+import s from "./home.module.css";
+import SocialMedia from "../components/SocialMedia/SocialMedia";
+import About from "../components/About/About";
 
 const HomePage = () => {
   return (
     <section className="py-4">
-      <Carousel />
+      <div className="row">
+        <div className="col-12 col-md-4 h-25">
+          <img
+            className={s.img}
+            src={require("../assets/images/photo.jpg")}
+            alt="Vitalii"
+          />
+          <h3 className="my-3 text-center">Dr. Vitalii Palchykov</h3>
+          <SocialMedia />
+        </div>
+        <div className="col-12 col-md-8">
+          <About />
+        </div>
+      </div>
     </section>
   );
 };
