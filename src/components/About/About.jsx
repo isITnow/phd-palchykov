77ad03ad1c3529346e3cf100315e67e44d2d3f09
Roman Serediuk <box4rom@gmail.com>
2023-03-22 21:53:16 +0200

    add strong to years

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,93 +7,100 @@ const About = () => {
       <ul>
         <li>
           <p className="mb-2">
-            2019 D.Sc.(doctor habil.), V.N. Karazin Kharkiv National University.
-            Supervisor: Prof., Dr. Valentyn Chebanov, thesis: Oxazaheterocycles
-            and opened heteroatomic compounds from epoxy derivatives
+            <strong>2019</strong> D.Sc.(doctor habil.), V.N. Karazin Kharkiv
+            National University. Supervisor: Prof., Dr. Valentyn Chebanov,
+            thesis: Oxazaheterocycles and opened heteroatomic compounds from
+            epoxy derivatives
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2008 Ph.D, Ukrainian State University of Chemical Technology.
-            Supervisor: Prof., Dr. Liliya Kasyan, thesis: Oxygen- and nitrogen
-            containing derivatives of
+            <strong>2008</strong> Ph.D, Ukrainian State University of Chemical
+            Technology. Supervisor: Prof., Dr. Liliya Kasyan, thesis: Oxygen-
+            and nitrogen containing derivatives of
             bicyclo[2.2.1]hept-5-ene-endo,endo-2,3-dicarboxylic acid anhydride.
             Synthesis and properties
           </p>
         </li>
         <li>
-          <p className="mb-2">2004 M.Sc, Dnipropetrovsk National University</p>
+          <p className="mb-2">
+            <strong>2004</strong> M.Sc, Dnipropetrovsk National University
+          </p>
         </li>
         <li>
-          <p className="mb-2">2003 B.Sc, Dnipropetrovsk National University</p>
+          <p className="mb-2">
+            <strong>2003</strong> B.Sc, Dnipropetrovsk National University
+          </p>
         </li>
       </ul>
       <h4>Research and Teaching Experience</h4>
       <ul>
         <li>
           <p className="mb-2">
-            2019 - present Director of the Research Institute of Chemistry and
-            Geology, DNU
+            <strong>2019</strong> - present Director of the Research Institute
+            of Chemistry and Geology, DNU
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2021 - present Full Professor (<em>part time</em>), Department of
-            Physical, Organic and Inorganic Chemistry, DNU
+            <strong>2021</strong> - present Full Professor (<em>part time</em>),
+            Department of Physical, Organic and Inorganic Chemistry, DNU
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2020 - 2021 Research Associate in Prof. Gevorgyan group, University
-            of Texas at Dallas, United States{" "}
+            <strong>2020 - 2021</strong> Research Associate in Prof. Gevorgyan
+            group, University of Texas at Dallas, United States{" "}
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2019 Visiting Researcher in Prof. Šebesta laboratory of asymmetric
-            catalysis and green chemistry, Comenius University in Bratislava,
-            Slovak Republic
+            <strong>2019</strong> Visiting Researcher in Prof. Šebesta
+            laboratory of asymmetric catalysis and green chemistry, Comenius
+            University in Bratislava, Slovak Republic
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2016 - 2019 Head of the Research Laboratory of theoretical and
-            applied problems of chemistry, Department of Organic Chemistry, DNU
+            <strong>2016 - 2019</strong> Head of the Research Laboratory of
+            theoretical and applied problems of chemistry, Department of Organic
+            Chemistry, DNU
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2017 - 2019 Assistant Professor (<em>part time</em>), Department of
+            <strong>2017 - 2019</strong> Assistant Professor (<em>part time</em>
+            ), Department of Organic Chemistry, DNU
+          </p>
+        </li>
+        <li>
+          <p className="mb-2">
+            <strong>2008 - 2016</strong> Senior Research Fellow, Department of
             Organic Chemistry, DNU
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2008 - 2016 Senior Research Fellow, Department of Organic Chemistry,
-            DNU
+            <strong>2012 - 2013</strong> Visiting Researcher in Prof. Robertson
+            group, University of Oxford, UK
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2012 - 2013 Visiting Researcher in Prof. Robertson group, University
-            of Oxford, UK
-          </p>
-        </li>
-        <li>
-          <p className="mb-2">
-            2007 - 2008 Junior Research Fellow, Department of Organic Chemistry,
-            DNU
-          </p>
-        </li>
-        <li>
-          <p className="mb-2">
-            2008 - 2011 Assistant Professor (<em>part time</em>), Department of
+            <strong>2007 - 2008</strong> Junior Research Fellow, Department of
             Organic Chemistry, DNU
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2004 - 2007 PhD student, Department of Organic Chemistry, DNU
+            <strong>2008 - 2011</strong> Assistant Professor (<em>part time</em>
+            ), Department of Organic Chemistry, DNU
+          </p>
+        </li>
+        <li>
+          <p className="mb-2">
+            <strong>2004 - 2007</strong> PhD student, Department of Organic
+            Chemistry, DNU
           </p>
         </li>
       </ul>
@@ -101,107 +108,112 @@ const About = () => {
       <ul>
         <li>
           <p className="mb-2">
-            2022 Full Professor's Certificate (Ministry of Education and Science
-            of Ukraine){" "}
-          </p>
-        </li>
-        <li>
-          <p className="mb-2">2022 Simons Foundation Scholarship (USA)</p>
-        </li>
-        <li>
-          <p className="mb-2">
-            2022 Prize of the Verkhovna Rada (Parliament) of Ukraine for young
-            scientists
+            <strong>2022</strong> Full Professor's Certificate (Ministry of
+            Education and Science of Ukraine){" "}
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2021 Grant for young scientists of Dnipropetrovsk region. Project
-            title: Genipin - a natural product for the discovery of new
-            practically useful materials
+            <strong>2022</strong> Simons Foundation Scholarship (USA)
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2021 Scholarship of the Verkhovna Rada (Parliament) of Ukraine for
-            young scientists - Doctor of Sciences
+            <strong>2022</strong> Prize of the Verkhovna Rada (Parliament) of
+            Ukraine for young scientists
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2020 Matsumae Research Fellowship (Kyoto University, Japan)
+            <strong>2021</strong> Grant for young scientists of Dnipropetrovsk
+            region. Project title: Genipin - a natural product for the discovery
+            of new practically useful materials
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2019 Travel grant for the 6th Prague-Weizmann Summer School
-            “Advances in Drug Discovery” (Prague, Czech Republic)
+            <strong>2021</strong> Scholarship of the Verkhovna Rada (Parliament)
+            of Ukraine for young scientists - Doctor of Sciences
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2019 Grant for the staff training within the framework of Erasmus+
-            KA107 project at the Karamanoğlu Mehmetbey University (Karaman,
-            Turkey)
+            <strong>2020</strong> Matsumae Research Fellowship (Kyoto
+            University, Japan)
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2018 Research Scholarship of the National Scholarship Programme of
-            the Slovak Republic. Supervisor: Prof. Radovan Šebesta (Comenius
-            University in Bratislava)
+            <strong>2019</strong> Travel grant for the 6th Prague-Weizmann
+            Summer School “Advances in Drug Discovery” (Prague, Czech Republic)
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2015 Prize of the President of Ukraine for young scientists for a
-            series of scientific papers (the most prestigious award in Ukraine
-            for scientists under 35){" "}
+            <strong>2019</strong> Grant for the staff training within the
+            framework of Erasmus+ KA107 project at the Karamanoğlu Mehmetbey
+            University (Karaman, Turkey)
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2015 Grant for young scientists of Dnipropetrovsk region. Project
-            title: The synthesis of natural antithrombotic agent
+            <strong>2018</strong> Research Scholarship of the National
+            Scholarship Programme of the Slovak Republic. Supervisor: Prof.
+            Radovan Šebesta (Comenius University in Bratislava)
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2014 Scholarship of the Cabinet of Ministers of Ukraine for young
-            scientists
+            <strong>2015</strong> Prize of the President of Ukraine for young
+            scientists for a series of scientific papers (the most prestigious
+            award in Ukraine for scientists under 35){" "}
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2013 Grant for young scientists of Dnipropetrovsk region. Project
-            title: Directed synthesis of biologically important heterocyclic
-            structures based on industrially available dienes
+            <strong>2015</strong> Grant for young scientists of Dnipropetrovsk
+            region. Project title: The synthesis of natural antithrombotic agent
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2013 Grant of the President of Ukraine for young scientists. Project
-            title: New heterocyclic compounds, annulated or substituted of
-            polycyclic cage fragments. Synthesis and in vivo activity
+            <strong>2014</strong> Scholarship of the Cabinet of Ministers of
+            Ukraine for young scientists
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2012 Grant of Ukrainian Ministry of Education and Science on
-            internship at the University of Oxford, UK. Supervisor: Prof. Jeremy
-            Robertson
+            <strong>2013</strong> Grant for young scientists of Dnipropetrovsk
+            region. Project title: Directed synthesis of biologically important
+            heterocyclic structures based on industrially available dienes
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2010, 2013, 2015 Best Young Scientist of Year in Dnipropetrovsk
-            region
+            <strong>2013</strong> Grant of the President of Ukraine for young
+            scientists. Project title: New heterocyclic compounds, annulated or
+            substituted of polycyclic cage fragments. Synthesis and in vivo
+            activity
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2004 President of Ukraine Scholarship for outstanding students 1999
-            Winner of the 38th Regional Chemistry Olympiad, Dnipropetrovsk
+            <strong>2012</strong> Grant of Ukrainian Ministry of Education and
+            Science on internship at the University of Oxford, UK. Supervisor:
+            Prof. Jeremy Robertson
+          </p>
+        </li>
+        <li>
+          <p className="mb-2">
+            <strong>2010</strong>, <strong>2013</strong>, <strong>2015</strong>{" "}
+            Best Young Scientist of Year in Dnipropetrovsk region
+          </p>
+        </li>
+        <li>
+          <p className="mb-2">
+            <strong>2004</strong> President of Ukraine Scholarship for
+            outstanding students 1999 Winner of the 38th Regional Chemistry
+            Olympiad, Dnipropetrovsk
           </p>
         </li>
       </ul>
@@ -209,18 +221,20 @@ const About = () => {
       <ul>
         <li>
           <p className="mb-2">
-            2022 - present Affiliate member of the Royal Society of Chemistry,
-            UK
+            <strong>2022</strong> - present Affiliate member of the Royal
+            Society of Chemistry, UK
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2013 - present Member of Oxford University Ukrainian Society{" "}
+            <strong>2013</strong> - present Member of Oxford University
+            Ukrainian Society{" "}
           </p>
         </li>
         <li>
           <p className="mb-2">
-            2015 - 2017 Chairman of the Young Scientists Council of DNU
+            <strong>2015 - 2017</strong> Chairman of the Young Scientists
+            Council of DNU
           </p>
         </li>
       </ul>
