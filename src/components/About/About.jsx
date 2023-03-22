ed6c759e502d1ec4367fdbeb60808dd560ed986b
Roman Serediuk <box4rom@gmail.com>
2023-03-22 20:57:37 +0200

    add About component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <h3>Education:</h3>
+      <p>
+        2019 D.Sc.(doctor habil.), V.N. Karazin Kharkiv National University.
+        Supervisor: Prof., Dr. Valentyn Chebanov, thesis: Oxazaheterocycles and
+        opened heteroatomic compounds from epoxy derivatives 2008 Ph.D,
+        Ukrainian State University of Chemical Technology. Supervisor: Prof.,
+        Dr. Liliya Kasyan, thesis: Oxygen- and nitrogen containing derivatives
+        of bicyclo[2.2.1]hept-5-ene-endo,endo-2,3-dicarboxylic acid anhydride.
+        Synthesis and properties 2004 M.Sc, Dnipropetrovsk National University
+        2003 B.Sc, Dnipropetrovsk National University
+      </p>
+      <h3>Research and Teaching Experience:</h3>
+      <p>
+        2019 – present Director of the Research Institute of Chemistry and
+        Geology, DNU 2021 – present Full Professor (part time), Department of
+        Physical, Organic and Inorganic Chemistry, DNU 2020 – 2021 Research
+        Associate in Prof. Gevorgyan group, University of Texas at Dallas,
+        United States 2019 Visiting Researcher in Prof. Šebesta laboratory of
+        asymmetric catalysis and green chemistry, Comenius University in
+        Bratislava, Slovak Republic 2016 – 2019 Head of the Research Laboratory
+        of theoretical and applied problems of chemistry, Department of Organic
+        Chemistry, DNU 2017 – 2019 Assistant Professor (part time), Department
+        of Organic Chemistry, DNU 2008 – 2016 Senior Research Fellow, Department
+        of Organic Chemistry, DNU 2012 – 2013 Visiting Researcher in Prof.
+        Robertson group, University of Oxford, UK 2007 – 2008 Junior Research
+        Fellow, Department of Organic Chemistry, DNU 2008 – 2011 Assistant
+        Professor (part time), Department of Organic Chemistry, DNU 2004 – 2007
+        PhD student, Department of Organic Chemistry, DNU
+      </p>
+      <h3>Awards and Achievements:</h3>
+      2022 Full Professor’s Certificate (Ministry of Education and Science of
+      Ukraine) 2022 Simons Foundation Scholarship (USA) 2022 Prize of the
+      Verkhovna Rada (Parliament) of Ukraine for young scientists 2021 Grant for
+      young scientists of Dnipropetrovsk region. Project title: Genipin ‒ a
+      natural product for the discovery of new practically useful materials 2021
+      Scholarship of the Verkhovna Rada (Parliament) of Ukraine for young
+      scientists – Doctor of Sciences 2020 Matsumae Research Fellowship (Kyoto
+      University, Japan) 2019 Travel grant for the 6th Prague-Weizmann Summer
+      School “Advances in Drug Discovery” (Prague, Czech Republic) 2019 Grant
+      for the staff training within the framework of Erasmus+ KA107 project at
+      the Karamanoğlu Mehmetbey University (Karaman, Turkey). 2018 Research
+      Scholarship of the National Scholarship Programme of the Slovak Republic.
+      Supervisor: Prof. Radovan Šebesta (Comenius University in Bratislava) 2015
+      Prize of the President of Ukraine for young scientists for a series of
+      scientific papers (the most prestigious award in Ukraine for scientists
+      under 35) 2015 Grant for young scientists of Dnipropetrovsk region.
+      Project title: The synthesis of natural antithrombotic agent 2014
+      Scholarship of the Cabinet of Ministers of Ukraine for young scientists
+      2013 Grant for young scientists of Dnipropetrovsk region. Project title:
+      Directed synthesis of biologically important heterocyclic structures based
+      on industrially available dienes 2013 Grant of the President of Ukraine
+      for young scientists. Project title: New heterocyclic compounds, annulated
+      or substituted of polycyclic cage fragments. Synthesis and in vivo
+      activity 2012 Grant of Ukrainian Ministry of Education and Science on
+      internship at the University of Oxford, UK. Supervisor: Prof. Jeremy
+      Robertson 2010, 2013, 2015 Best Young Scientist of Year in Dnipropetrovsk
+      region 2004 President of Ukraine Scholarship for outstanding students 1999
+      Winner of the 38th Regional Chemistry Olympiad, Dnipropetrovsk
+      Professional activity: 2022 – present Affiliate member of the Royal
+      Society of Chemistry, UK 2013 – present Member of Oxford University
+      Ukrainian Society 2015 – 2017 Chairman of the Young Scientists Council of
+      DNU Research Interests: chemistry of cage-like compounds and
+      N,O,S-heterocycles, new synthetic methodologies based on organo- and
+      photoredox catalysis, mechanisms and stereochemistry of organic reactions,
+      synthesis of biologically active and natural compounds My profiles:
+      ResearchGate https://www.researchgate.net/profile/Vitalii_Palchykov ORCID
+      http://orcid.org/0000-0003-3748-4566 Scopus
+      http://www.scopus.com/authid/detail.url?authorId=10139119000 Web of
+      Science http://www.researcherid.com/rid/C-8035-2016 Facebook
+      https://www.facebook.com/profile.php?id=100011011327376 LinkedIn
+      www.linkedin.com/in/vitalii-palchykov-1350a116b Google Scholar
+      https://scholar.google.com/citations?user=aZbFei8AAAAJ&hl=ru
+    </>
+  );
+};
+
+export default About;
