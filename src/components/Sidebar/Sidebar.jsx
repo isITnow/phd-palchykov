01d0f187ff2a8954abb3834e7583f835903860e9
Roman Serediuk <box4rom@gmail.com>
2023-03-23 10:21:53 +0200

    add Sidebar component, import QrCode SocialMedia components

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,20 @@
+import QrCode from "../QRCode/QrCode";
+import SocialMedia from "../SocialMedia/SocialMedia";
+import s from "./sidebar.module.css";
+import photo from "../../assets/images/photo.jpg";
+
+const Sidebar = () => {
+  return (
+    <>
+      <div className="mb-4">
+        <img className={s.img} src={photo} alt="Vitalii Palchykov at work" />
+      </div>
+      <div className="d-flex justify-content-center">
+        <SocialMedia />
+      </div>
+      <QrCode />
+    </>
+  );
+};
+
+export default Sidebar;
