edccd1e39ff546c5a189c4aeade8fffb9d85a3d9
Roman Serediuk <box4rom@gmail.com>
2023-03-23 10:24:22 +0200

    import About Sidebar components

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,21 +1,12 @@
-import s from "./home.module.css";
-import SocialMedia from "../components/SocialMedia/SocialMedia";
 import About from "../components/About/About";
+import Sidebar from "../components/Sidebar/Sidebar";
 
 const HomePage = () => {
   return (
     <section className="py-4">
       <div className="row">
         <div className="col-12 col-md-4 h-25 mb-5">
-          <img
-            className={s.img}
-            src={require("../assets/images/photo.jpg")}
-            alt="Vitalii"
-          />
-          <h4 className="my-3 text-center">Prof. Dr. Vitalii Palchykov</h4>
-          <div className="d-flex justify-content-center">
-            <SocialMedia />
-          </div>
+          <Sidebar />
         </div>
         <div className="col-12 col-md-8">
           <About />
