dad842442af6259aeee7777df9703d08c883a4d9
Roman Serediuk <box4rom@gmail.com>
2023-03-30 14:49:29 +0300

    add News page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from "./components/Layout";
 import ContactsPage from "./pages/ContactsPage";
 import GalleryPage from "./pages/GalleryPage";
 import HomePage from "./pages/HomePage";
+import NewsPage from "./pages/NewsPage";
 import PublicationsPage from "./pages/PublicationsPage";
 import ResearchPage from "./pages/ResearchPage";
 
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="publications/:period" element={<PublicationsPage />} />
         <Route path="research" element={<ResearchPage />} />
         <Route path="gallery/:theme" element={<GalleryPage />} />
+        <Route path="news" element={<NewsPage />} />
         <Route path="contacts" element={<ContactsPage />} />
       </Route>
     </Routes>
