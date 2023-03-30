445909c821f50a5ee3588dba4725dba3d01701d7
Roman Serediuk <box4rom@gmail.com>
2023-03-30 16:37:47 +0300

    create NewsItem

---
+++ src/components/NewsItem/NewsItem.jsx
@@ -0,0 +1,15 @@
+import s from "./newsItem.module.css";
+
+const NewsItem = () => {
+  return (
+    <div>
+      <h4>News title</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+        minima!
+      </p>
+    </div>
+  );
+};
+
+export default NewsItem;
