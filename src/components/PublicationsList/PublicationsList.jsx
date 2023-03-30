0c4bcf717413d51410a752fae978d639fee9fbe5
Roman Serediuk <box4rom@gmail.com>
2023-03-30 18:50:58 +0300

    get appropriate list and render list of Publication components

--- src/components/PublicationsList/PublicationsList.jsx
+++ src/components/PublicationsList/PublicationsList.jsx
@@ -1,24 +1,26 @@
 import Publication from "../Publication/Publication";
 import s from "./publicationsList.module.css";
+import publicationsArray from "../../assets/data/publications";
+import { useEffect, useState } from "react";
 
-const PublicationsList = () => {
+const PublicationsList = ({ period }) => {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    const data = publicationsArray.find(
+      (publication) => publication.period === period
+    ).publicationsList;
+    setList(data);
+  }, [period]);
+
   return (
-    <>
-      <ul className="row row-cols-1 row-cols-md-2 g-2">
-        <li className={`col ${s.hoverEffect}`}>
-          <Publication />
+    <ul className="row row-cols-1 row-cols-md-2 g-2">
+      {list.map((publication) => (
+        <li key={publication.id} className={`col ${s.hoverEffect}`}>
+          <Publication publication={publication} />
         </li>
-        <li className="col">
-          <Publication />
-        </li>
-        <li className="col">
-          <Publication />
-        </li>
-        <li className="col">
-          <Publication />
-        </li>
-      </ul>
-    </>
+      ))}
+    </ul>
   );
 };
 
