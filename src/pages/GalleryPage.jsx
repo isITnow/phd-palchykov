cbfb137ebcd3b2a004b95e738dd40b5037996d47
Roman Serediuk <box4rom@gmail.com>
2023-03-31 16:10:07 +0300

    import Gallery component

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -1,11 +1,12 @@
 import { useParams } from "react-router-dom";
+import Gallery from "../components/Gallery/Gallery";
 
 const GalleryPage = () => {
   const { theme } = useParams();
   return (
     <section className="py-4">
-      <h2>GalleryPage</h2>
-      <h4>{theme}</h4>
+      <h2>Gallery Page</h2>
+      <Gallery theme={theme} />
     </section>
   );
 };
