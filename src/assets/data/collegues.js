af505d9bffc74866cd0913f0e04819ad4f533301
Roman Serediuk <box4rom@gmail.com>
2023-04-03 15:09:59 +0300

    add some colleagues

---
+++ src/assets/data/collegues.js
@@ -0,0 +1,18 @@
+const colleaguesArray = [
+  {
+    id: 1,
+    name: 'Richard "Rick" Sanchez',
+    position: "Head of the Lab at Umbrella Ltd.",
+    contacts: "+99 000 88 88",
+    photo: "rick.webp",
+  },
+  {
+    id: 2,
+    name: "Algernop Krieger",
+    position: "Head of the ISIS Applied Research Department",
+    contacts: "+99 000 88 88",
+    photo: "kriger.jpeg",
+  },
+];
+
+export default colleaguesArray;
