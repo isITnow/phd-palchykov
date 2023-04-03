c3f795000c0ff6db3d132945422a34772fca30d9
Roman Serediuk <box4rom@gmail.com>
2023-04-03 11:04:03 +0300

    add card structure

--- src/components/NewsItem/NewsItem.jsx
+++ src/components/NewsItem/NewsItem.jsx
@@ -2,12 +2,24 @@
 
 const NewsItem = () => {
   return (
-    <div>
-      <h4>News title</h4>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
-        minima!
-      </p>
+    // <div>
+    //   <h5 className="card-title text-danger">News title</h5>
+    //   <p>
+    //     Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+    //     minima!
+    //   </p>
+    // </div>
+
+    <div class="card text-center">
+      <div class="card-header">
+        <h5 class="card-title">Special title treatment</h5>
+      </div>
+      <div class="card-body">
+        <p class="card-text">
+          With supporting text below as a natural lead-in to additional content.
+        </p>
+      </div>
+      <div class="card-footer text-body-secondary">2 days ago</div>
     </div>
   );
 };
