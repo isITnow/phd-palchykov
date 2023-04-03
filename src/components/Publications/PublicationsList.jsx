0a4cbd87208697680132f0a53cb7c271adbf32cf
Roman Serediuk <box4rom@gmail.com>
2023-04-03 15:43:49 +0300

    move get publications list logic to PublicationsPage

--- src/components/Publications/PublicationsList.jsx
+++ src/components/Publications/PublicationsList.jsx
@@ -1,20 +1,9 @@
 import Publication from "./Publication";
-import publicationsArray from "../../assets/data/publications";
-import { useEffect, useState } from "react";
 
-const PublicationsList = ({ period }) => {
-  const [list, setList] = useState([]);
-
-  useEffect(() => {
-    const data = publicationsArray.find(
-      (publication) => publication.period === period
-    ).publicationsList;
-    setList(data);
-  }, [period]);
-
+const PublicationsList = ({ publications }) => {
   return (
     <ul className="row row-cols-1 row-cols-md-2 g-2">
-      {list.map((publication) => (
+      {publications.map((publication) => (
         <li key={publication.id} className="col">
           <Publication publication={publication} />
         </li>
--- src/pages/PublicationsPage.jsx
+++ src/pages/PublicationsPage.jsx
@@ -1,12 +1,23 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import publicationsArray from "../assets/data/publications";
 import PublicationsList from "../components/Publications/PublicationsList";
 
 const PublicationsPage = () => {
   const { period } = useParams();
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    const data = publicationsArray.find(
+      (publication) => publication.period === period
+    ).publicationsList;
+    setList(data);
+  }, [period]);
+
   return (
     <section className="py-4">
       <p className="mb-3 text-secondary text-end fw-bold">{period}</p>
-      <PublicationsList period={period} />
+      <PublicationsList publications={list} />
     </section>
   );
 };
