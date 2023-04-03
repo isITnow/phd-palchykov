59e41e6adc602977890909e23075892278389565
Roman Serediuk <box4rom@gmail.com>
2023-04-03 18:36:14 +0300

    add draft data

--- src/assets/data/news.js
+++ src/assets/data/news.js
@@ -1,5 +1,13 @@
 const newsArray = [
   {
+    id: 2,
+    title: "Emu captured after four days on the loose in New Zealand",
+    description:
+      "March 31 (UPI) -- Animal rescuers in New Zealand said an escaped emu returned home after being chased around the area for four days. Summer Johnson, founder of Bay of Islands Animal Rescue, said she was contacted Saturday by a resident who reported their emu had escaped and was wandering around the town of Kawakawa, in Northland.",
+    img: "",
+    date: "April 3, 2023",
+  },
+  {
     id: 1,
     title: "A new specialty at the Oles Honchar Dnipro National University",
     description: "",
