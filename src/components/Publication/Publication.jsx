d81cc84eecb74803e2da0e10430a0f445bd1d645
Roman Serediuk <box4rom@gmail.com>
2023-04-03 10:29:16 +0300

    render actual data

--- src/components/Publication/Publication.jsx
+++ src/components/Publication/Publication.jsx
@@ -10,21 +10,24 @@ const Publication = ({ publication }) => {
           <span className="me-2 text-secondary fs-5 lh-sm">{id}.</span>
           <h5 className="card-title text-danger">{title}</h5>
         </div>
-        <p className="card-subtitle">{authors.join("; ")}</p>
-        <a
-          href={url}
-          target="_blank"
-          rel="noreferrer noopener"
-          className="fst-italic"
-        >
-          {source}
+        <p className="card-subtitle mt-2">{authors.join("; ")}</p>
+        <a href={url} target="_blank" rel="noreferrer noopener">
+          <p className="fst-italic mt-2">{source}</p>
         </a>
-        <div className="row row-cols-1 row-cols-md-2 mt-3">
-          <div className="col">
-            <img src={cover_img} className={s.img} alt="..." />
+        <div className="row row-cols-1 mt-3">
+          <div className="col mt-2">
+            <img
+              src={require(`../../assets/images/covers/${abstract_img}`)}
+              className={s.img_formula}
+              alt="..."
+            />
           </div>
-          <div className="col mt-1 mt-sm-0">
-            <img src={abstract_img} className={s.img_formula} alt="..." />
+          <div className="col mt-2">
+            <img
+              src={require(`../../assets/images/covers/${cover_img}`)}
+              className={s.img}
+              alt="..."
+            />
           </div>
         </div>
       </div>
