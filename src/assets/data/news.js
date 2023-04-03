f0470e658ea22c24046f0712c26268b6bf286b24
Roman Serediuk <box4rom@gmail.com>
2023-04-03 11:56:55 +0300

    data upd

--- src/assets/data/news.js
+++ src/assets/data/news.js
@@ -3,10 +3,9 @@ const newsArray = [
     id: 1,
     title: "A new specialty at the Oles Honchar Dnipro National University",
     description: "",
-    img: "news_cover-2023-04",
+    img: "news_cover-2023-04.jpg",
     date: "April 1, 2023",
   },
-  {},
 ];
 
 export default newsArray;
