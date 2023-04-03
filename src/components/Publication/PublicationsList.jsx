e86b5467924310c4071d70f35a0c274b09aaca8b
Roman Serediuk <box4rom@gmail.com>
2023-04-03 11:31:10 +0300

    remove styles import

--- src/components/Publication/PublicationsList.jsx
+++ src/components/Publication/PublicationsList.jsx
@@ -1,5 +1,4 @@
 import Publication from "./Publication";
-import s from "./publicationsList.module.css";
 import publicationsArray from "../../assets/data/publications";
 import { useEffect, useState } from "react";
 
@@ -16,7 +15,7 @@ const PublicationsList = ({ period }) => {
   return (
     <ul className="row row-cols-1 row-cols-md-2 g-2">
       {list.map((publication) => (
-        <li key={publication.id} className={`col ${s.hoverEffect}`}>
+        <li key={publication.id} className="col">
           <Publication publication={publication} />
         </li>
       ))}
