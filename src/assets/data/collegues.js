733373ad631d7a11ee3f3d5e90dc0c4e30dd7c16
Roman Serediuk <box4rom@gmail.com>
2023-04-04 19:44:39 +0300

    upd: items order

--- src/assets/data/collegues.js
+++ src/assets/data/collegues.js
@@ -1,21 +1,5 @@
 const colleaguesArray = [
   {
-    id: 2,
-    name: "Yevhenii Kozyriev",
-    position: "3rd year PhD student",
-    phone: "+380966754867",
-    email: "eugenekozirev@gmail.com",
-    photo: "Yevhenii_Kozyriev.jpg",
-  },
-  {
-    id: 4,
-    name: "Kyrylo Tymoshenko",
-    position: "1st year PhD student",
-    phone: "+380664944791",
-    email: "kirilt650i2@gmail.com",
-    photo: "Kyrylo_Tymoshenko.jpg",
-  },
-  {
     id: 1,
     name: "Kateryna Dil",
     position: "4th year PhD student",
@@ -24,12 +8,28 @@ const colleaguesArray = [
     photo: "Kateryna_Dil.jpg",
   },
   {
+    id: 2,
+    name: "Yevhenii Kozyriev",
+    position: "3rd year PhD student",
+    phone: "+380966754867",
+    email: "eugenekozirev@gmail.com",
+    photo: "Yevhenii_Kozyriev.jpg",
+  },
+  {
     id: 3,
     name: "Olga Shevchenko",
     position: "2nd year PhD student",
     phone: "+380968179788",
     email: "tigra8olchik@gmail.com",
     photo: "Olga_Shevchenko.jpg",
+  },
+  {
+    id: 4,
+    name: "Kyrylo Tymoshenko",
+    position: "1st year PhD student",
+    phone: "+380664944791",
+    email: "kirilt650i2@gmail.com",
+    photo: "Kyrylo_Tymoshenko.jpg",
   },
 ];
 
