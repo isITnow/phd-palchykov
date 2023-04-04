8712a63d6adab55d958ff5cd1ae0e96b6541bb0c
Roman Serediuk <box4rom@gmail.com>
2023-04-04 18:39:20 +0300

    add actual cilleagues info

--- src/assets/data/collegues.js
+++ src/assets/data/collegues.js
@@ -1,17 +1,35 @@
 const colleaguesArray = [
   {
     id: 1,
-    name: 'Richard "Rick" Sanchez',
-    position: "Head of the Lab at Umbrella Ltd.",
-    contacts: "+99 000 88 88",
-    photo: "rick.webp",
+    name: "Kateryna Dil",
+    position: "4th year PhD student",
+    contacts: "+38 098 7669259",
+    email: "katerina.vlodimirovna@ukr.net",
+    photo: "Kateryna_Dil.jpg",
   },
   {
     id: 2,
-    name: "Algernop Krieger",
-    position: "Head of the ISIS Applied Research Department",
-    contacts: "+99 000 88 88",
-    photo: "kriger.jpeg",
+    name: "Yevhenii Kozyriev",
+    position: "3rd year PhD student",
+    contacts: "+38 096 6754867",
+    email: "eugenekozirev@gmail.com",
+    photo: "Yevhenii_Kozyriev.jpg",
+  },
+  {
+    id: 3,
+    name: "Olga Shevchenko",
+    position: "2nd year PhD student",
+    contacts: "+38 096 8179788",
+    email: "tigra8olchik@gmail.com",
+    photo: "Olga_Shevchenko.jpg",
+  },
+  {
+    id: 4,
+    name: "Kyrylo Tymoshenko",
+    position: "1st year PhD student",
+    contacts: "+38 066 4944791",
+    email: "kirilt650i2@gmail.com",
+    photo: "Kyrylo_Tymoshenko.jpg",
   },
 ];
 
