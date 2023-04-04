7165da760b6680326ec79fdc4eee7238efec62e8
Roman Serediuk <box4rom@gmail.com>
2023-04-04 18:54:12 +0300

    fix: rename contacts to phone

--- src/assets/data/collegues.js
+++ src/assets/data/collegues.js
@@ -1,35 +1,35 @@
 const colleaguesArray = [
   {
-    id: 1,
-    name: "Kateryna Dil",
-    position: "4th year PhD student",
-    contacts: "+38 098 7669259",
-    email: "katerina.vlodimirovna@ukr.net",
-    photo: "Kateryna_Dil.jpg",
-  },
-  {
     id: 2,
     name: "Yevhenii Kozyriev",
     position: "3rd year PhD student",
-    contacts: "+38 096 6754867",
+    phone: "+380966754867",
     email: "eugenekozirev@gmail.com",
     photo: "Yevhenii_Kozyriev.jpg",
-  },
-  {
-    id: 3,
-    name: "Olga Shevchenko",
-    position: "2nd year PhD student",
-    contacts: "+38 096 8179788",
-    email: "tigra8olchik@gmail.com",
-    photo: "Olga_Shevchenko.jpg",
   },
   {
     id: 4,
     name: "Kyrylo Tymoshenko",
     position: "1st year PhD student",
-    contacts: "+38 066 4944791",
+    phone: "+380664944791",
     email: "kirilt650i2@gmail.com",
     photo: "Kyrylo_Tymoshenko.jpg",
+  },
+  {
+    id: 1,
+    name: "Kateryna Dil",
+    position: "4th year PhD student",
+    phone: "+380987669259",
+    email: "katerina.vlodimirovna@ukr.net",
+    photo: "Kateryna_Dil.jpg",
+  },
+  {
+    id: 3,
+    name: "Olga Shevchenko",
+    position: "2nd year PhD student",
+    phone: "+380968179788",
+    email: "tigra8olchik@gmail.com",
+    photo: "Olga_Shevchenko.jpg",
   },
 ];
 
