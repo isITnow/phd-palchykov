b83294dd2c8847b6762a94762a3f526dadb476f5
Roman Serediuk <box4rom@gmail.com>
2023-04-04 18:55:23 +0300

    feat: add list of contacts

--- src/components/Colleagues/Colleague.jsx
+++ src/components/Colleagues/Colleague.jsx
@@ -1,5 +1,5 @@
 const Colleague = ({ colleague }) => {
-  const { name, position, photo, contacts } = colleague;
+  const { name, position, photo, phone, email } = colleague;
   return (
     <div className="card mb-3 overflow-hidden" style={{ maxWidth: 540 }}>
       <div className="row g-0">
@@ -14,7 +14,16 @@ const Colleague = ({ colleague }) => {
           <div className="card-body">
             <h5 className="card-title">{name}</h5>
             <p className="card-text">{position}</p>
-            <p className="card-text">{contacts}</p>
+            <ul>
+              <li>
+                <span className="me-2">email:</span>
+                <a href={`mailto:${email}`}>{email}</a>
+              </li>
+              <li>
+                <span className="me-2">phone:</span>
+                <a href={`tel:${phone}`}>{phone}</a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
