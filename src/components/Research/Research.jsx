fec34f8e508989bc41713081d55ac63c052b2fa6
Roman Serediuk <box4rom@gmail.com>
2023-04-07 12:24:45 +0300

    feat: add structure

--- src/components/Research/Research.jsx
+++ src/components/Research/Research.jsx
@@ -1,5 +1,39 @@
-const Research = () => {
-  return <div>Research</div>;
+const Research = ({ research }) => {
+  // console.log("research: ", research);
+  const { id, title, illustrations, sources } = research;
+  console.log("illustrations: ", illustrations);
+  return (
+    <div className="">
+      <div className="d-flex mb-2 justify-content-center">
+        <span className="me-2 text-secondary fs-5 lh-sm">{id}.</span>
+        <h5 className="card-title text-danger">{title}</h5>
+      </div>
+      {illustrations.map(({ img, description }, idx) => (
+        <div key={idx} className="mb-3">
+          <p className="" style={{ textIndent: 50 }}>
+            {description}
+          </p>
+          <div className="w-75 mx-auto">
+            <img
+              className="img-fluid"
+              src={require(`../../assets/images/research/${img}`)}
+              alt="..."
+            />
+          </div>
+        </div>
+      ))}
+      <p className="mb-2">Our relevant works:</p>
+      <ul className="ms-2">
+        {sources.map(({ url, source }, idx) => (
+          <li key={idx}>
+            <a href={url} target="_blank" rel="noreferrer noopener">
+              <span className="fst-italic">{source}</span>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Research;
