c8ca35b22a92b4c6c2bb2c0aae6048505c12e034
Roman Serediuk <box4rom@gmail.com>
2023-04-07 11:43:11 +0300

    feat: render ResearchList component

--- src/pages/ResearchPage.jsx
+++ src/pages/ResearchPage.jsx
@@ -1,8 +1,11 @@
+import ResearchList from "../components/Research/ResearchList";
+
 const ResearchPage = () => {
   return (
-    <>
+    <section className="py-4">
       <h2>Research</h2>
-    </>
+      <ResearchList />
+    </section>
   );
 };
 
