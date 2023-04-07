fb7c2818d02617bb9c57e61ae9074087fbec9fb4
Roman Serediuk <box4rom@gmail.com>
2023-04-07 10:33:20 +0300

    add reserch data

---
+++ src/assets/data/research.js
@@ -0,0 +1,55 @@
+const researchArray = [
+  {
+    id: 1,
+    title:
+      "Design of new biologically relevant heterocycles based on β-ketosulfones",
+    illustrations: [
+      {
+        img: "fig_1_1.jpg",
+        description:
+          "Cyclic sulfoxides and sulfones are important pharmacophores with a wide range of pharmacological activities owing to a range of mechanisms of action and are widely used in drug design [Adv. Het. Chem. 2021, 134, 1-30]. The cyclic sulfone moiety is frequently utilized in medicinal chemistry to optimize the physicochemical properties of lead compounds. The polarity associated with both sulfones and sulfoxides helps to lower the overall lipophilicity which in many cases translates into improvements in ADME and pharmaceutical properties. There are many examples of commercial drugs and clinical candidates containing six-membered thiopyrane ring. Depending on the substitution pattern of the core thiopyrane ring, this class of compounds has demonstrated a diverse range of biological activities ranging from anti-inflammatory and antiviral to ATP-sensitive potassium channel (KATP) openers. Many compounds are now under active investigation as selective inhibitors of different enzymes and kinases, and agonists/antagonists of receptors. Antiglaucoma agent Dorzolamide, diuretic Metikran and antiherpesvirus agent Amenamevir (ASP-2151) became marketed drugs",
+      },
+      {
+        img: "fig_1_2.jpg",
+        description:
+          "We turned our attention to β-ketosulfones since they have been established as versatile reagents useful for the preparation of a multitude of sulfur-containing compounds both synthetic and biological importance. In this context, cyclic β-ketosulfones are among the most promising reagents and particularly useful due to their availability and possible application in the synthesis of a diverse range of polycyclic sulfones. We are currently working on a green, mild, and scalable synthesis of S-heterocycles depicted below",
+      },
+    ],
+    sources: [
+      {
+        source: "Ukr. Biochem. J. 2022, 94(1), 64-74",
+        url: "https://doi.org/10.15407/ubj94.01.064",
+      },
+      {
+        source: "Synth. Commun. 2020, 50(12), 1835-1844",
+        url: "https://doi.org/10.1080/00397911.2020.1757113",
+      },
+      {
+        source: "Chem. Heteroc. Comp. 2019, 55(4-5), 349-351",
+        url: "https://doi.org/10.1007/s10593-019-02463-z",
+      },
+      {
+        source: "Synth. Commun. 2018, 48(17), 2198-2205",
+        url: "https://doi.org/10.1080/00397911.2018.1486427",
+      },
+      {
+        source: "Russ. J. Org. Chem. 2018, 54(4), 588-592",
+        url: "http://dx.doi.org/10.1134/S1070428018040127",
+      },
+      {
+        source: "New J. Chem. 2018, 42(2), 1403-1412",
+        url: "https://doi.org/10.1039/C7NJ03846A",
+      },
+      {
+        source: "Acta Chim. Slov. 2017, 64(4), 902-910",
+        url: "http://dx.doi.org/10.17344/acsi.2017.3575",
+      },
+      {
+        source: "Chem. Het. Comp. 2017, 53(10), 1134-1147",
+        url: "https://doi.org/10.1007/s10593-017-2184-8",
+      },
+    ],
+  },
+];
+
+export default researchArray;
