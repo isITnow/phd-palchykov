2d88c99a72ce97b682b7b652ddbefd8fd4452b58
Roman Serediuk <box4rom@gmail.com>
2023-04-07 15:41:58 +0300

    feat: implement PagesNav component

--- src/pages/PublicationsPage.jsx
+++ src/pages/PublicationsPage.jsx
@@ -1,23 +1,29 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import publicationsArray from "../assets/data/publications";
+import PagesNav from "../components/PagesNav";
 import PublicationsList from "../components/Publications/PublicationsList";
+import publicationsArray from "../assets/data/publications";
 
 const PublicationsPage = () => {
   const { period } = useParams();
   const [list, setList] = useState([]);
+  const periodsList = publicationsArray.map(
+    (publication) => publication.period
+  );
 
   useEffect(() => {
     const data = publicationsArray.find(
       (publication) => publication.period === period
     ).publicationsList;
     setList(data);
+    window.scrollTo(0, 0);
   }, [period]);
 
   return (
     <section className="py-4">
       <p className="mb-3 text-secondary text-end fw-bold">{period}</p>
       <PublicationsList publications={list} />
+      <PagesNav list={periodsList} location={period} />
     </section>
   );
 };
