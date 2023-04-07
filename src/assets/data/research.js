472ce29d67349317025ec82ec2cf4833be6ee0b1
Roman Serediuk <box4rom@gmail.com>
2023-04-07 11:18:35 +0300

    add second data item

--- src/assets/data/research.js
+++ src/assets/data/research.js
@@ -50,6 +50,121 @@ const researchArray = [
       },
     ],
   },
+  {
+    id: 2,
+    title: "Chemistry and biological activity of cage-like compounds",
+    illustrations: [
+      {
+        img: "fig_2_1.jpg",
+        description:
+          "Compounds with unusual three-dimensional structures have frequently attracted the attention of chemists as possible synthetic targets because the shape of chemical structures in drug discovery is a crucial component for evoking molecular recognition events with biological targets. Medicinal chemists typically exploit novel conformationally restricted 3D-shaped building blocks with a high fraction of C(sp3)-hybridized carbons (Fsp3). Polycyclic cage compounds have important pharmaceutical applications, ranging from the symptomatic and proposed curative treatment of neurodegenerative diseases such as Parkinson's and Alzheimer's disease, to use as antiviral agents against influenza and the immunodeficiency virus (HIV). The polycyclic cage appears to be a useful scaffold to yield drugs with a wide scope of applications and can be used also to modify and improve the pharmacokinetic and pharmacodynamic properties of drugs in current use.",
+      },
+      {
+        img: "fig_2_2.jpg",
+        description:
+          "It is not surprising, therefore, that currently medicinal chemists are increasingly searching for novel, unique, 3D-shaped, and conformationally restricted screening compounds. Thus, cage-like molecules are attractive for the drug discovery in terms of modern medical chemistry and new concepts, and therefore we set out to design and synthesize such derivatives that would be of interest as potential anti-bacterial, anti-viral, anti-fungal and anti-cancer agents. One of the best motifs in our chemistry are the various epoxynorbornanes, which give a wide range of interesting cage derivatives. ",
+      },
+      {
+        img: "fig_2_3.jpg",
+        description:
+          "We have also widely used other epoxides for the synthesis of target N,O-heterocycles annelated or substituted with polycyclic cage fragments.",
+      },
+    ],
+    sources: [
+      {
+        source: "Ukr. Biochem. J. 2022, 94(3), 68-80",
+        url: "https://doi.org/10.15407/ubj94.03.068",
+      },
+      {
+        source: "J. Chem. Technol. 2022, 30(1), 1-10",
+        url: "https://doi.org/10.15421/jchemtech.v30i1.246451",
+      },
+      {
+        source: "Med. Chem. Res. 2022, 31(2), 284-292",
+        url: "https://doi.org/10.1007/s00044-021-02827-1",
+      },
+      {
+        source: "Eur. J. Med. Chem. 2020, 204, 112617",
+        url: "https://doi.org/10.1016/j.ejmech.2020.112617",
+      },
+      {
+        source: "Tetrahedron Lett. 2020, 61(28), 152097",
+        url: "https://doi.org/10.1016/j.tetlet.2020.152097",
+      },
+      {
+        source: "J. Phys. Org. Chem. 2020, 33(10), e4071",
+        url: "https://doi.org/10.1002/poc.4071",
+      },
+      {
+        source: "Chem. Heteroc. Comp. 2020, 56(6), 793-799",
+        url: "https://doi.org/10.1007/s10593-020-02732-2",
+      },
+      {
+        source: "J. Het. Chem. 2018, 55(10), 2381-2391",
+        url: "https://doi.org/10.1002/jhet.3302",
+      },
+      {
+        source: "Russ. J. Org. Chem. 2018, 54(7), 1061-1070",
+        url: "http://dx.doi.org/10.1134/S1070428018070151",
+      },
+      {
+        source: "J. Mol. Struct. 2018, 1157, 149-158",
+        url: "https://doi.org/10.1016/j.molstruc.2017.12.055",
+      },
+      {
+        source: "Russ. J. Org. Chem. 2017, 53(5), 656-662",
+        url: "http://dx.doi.org/10.1134/S1070428017050037",
+      },
+      {
+        source: "Russ. J. Org. Chem. 2015, 51(8), 1114-1118",
+        url: "http://dx.doi.org/10.1134/S1070428015080096",
+      },
+      {
+        source: "Russ. J. Org. Chem. 2015, 51(8), 1199-1201",
+        url: "http://dx.doi.org/10.1134/S1070428015080278",
+      },
+      {
+        source: "Russ. J. Org. Chem. 2014, 50(9), 1312-1316",
+        url: "http://dx.doi.org/10.1134/S1070428014090139",
+      },
+      {
+        source: "Eur. Chem. Bull. 2014, 3(6), 543-547",
+        url: "http://dx.doi.org/10.17628/ecb.2014.3.543-547",
+      },
+      {
+        source: "Monatsh. Chem. 2014, 145(7), 1155-1163",
+        url: "http://dx.doi.org/10.1007/s00706-014-1192-y",
+      },
+      {
+        source: "Russ. J. Org. Chem. 2013, 49(8), 1122-1134",
+        url: "http://dx.doi.org/10.1134/S1070428013080058",
+      },
+      {
+        source: "Russ. J. Org. Chem. 2013, 49(5), 686-690",
+        url: "http://dx.doi.org/10.1134/S1070428013050084",
+      },
+      {
+        source: "Russ. J. Org. Chem. 2011, 47(7), 994-1002",
+        url: "http://dx.doi.org/10.1134/S1070428011070050",
+      },
+      {
+        source: "Russ. J. Org. Chem. 2011, 47(1), 74-82",
+        url: "http://dx.doi.org/10.1134/S1070428011010088",
+      },
+      {
+        source: "J. Phys. Org. Chem. 2011, 24(8), 705-713",
+        url: "http://dx.doi.org/10.1002/poc.1815",
+      },
+      {
+        source: "Org. Biomol. Chem. 2010, 8(9), 2142-2157",
+        url: "http://dx.doi.org/10.1039/b917850c",
+      },
+      {
+        source: "Russ. J. Org. Chem. 2006, 42(4), 501-508",
+        url: "http://dx.doi.org/10.1134/S1070428006040051",
+      },
+    ],
+  },
 ];
 
 export default researchArray;
