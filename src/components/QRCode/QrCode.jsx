8b1430fabcab70d4e90c34f39eb274b18ac08342
Roman Serediuk <box4rom@gmail.com>
2023-04-07 15:58:00 +0300

    delete qrCode.module.css

--- src/components/QRCode/QrCode.jsx
+++ src/components/QRCode/QrCode.jsx
@@ -1,15 +1,12 @@
-import s from "./qrCode.module.css";
 import qrCode from "../../assets/images/qr-code.jpg";
 
 const QrCode = () => {
   return (
     <div className="text-center">
       <div className="w-50 mx-auto">
-        <img className={s.img} src={qrCode} alt="ORCID" />
+        <img className="img-fluid" src={qrCode} alt="ORCID" />
       </div>
-      {/* <div> */}
       <span>my ORCID iD QR Code</span>
-      {/* </div> */}
     </div>
   );
 };
