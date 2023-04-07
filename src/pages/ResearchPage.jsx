3240649d2bfbb69714df38c59eae7ac02a5f5017
Roman Serediuk <box4rom@gmail.com>
2023-04-07 11:48:10 +0300

    remove title

--- src/pages/ResearchPage.jsx
+++ src/pages/ResearchPage.jsx
@@ -3,7 +3,6 @@ import ResearchList from "../components/Research/ResearchList";
 const ResearchPage = () => {
   return (
     <section className="py-4">
-      <h2>Research</h2>
       <ResearchList />
     </section>
   );
