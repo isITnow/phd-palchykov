c81d7ab11b842a071eae41195b5f7bb14f978c64
Roman Serediuk <box4rom@gmail.com>
2023-04-10 22:03:37 +0300

    feat: add handleClickScroll fn to implement scroll effect

--- src/components/Research/ResearchList.jsx
+++ src/components/Research/ResearchList.jsx
@@ -1,18 +1,36 @@
 import Research from "./Research";
 import researchesArray from "../../assets/data/researches";
+import s from "./research.module.css";
 
 const ResearchList = () => {
+  const handleClickScroll = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
-    <ul>
-      {researchesArray.map((research) => (
-        <li
-          className="mb-5 border-2 border-bottom border-danger pb-3"
-          key={research.id}
-        >
-          <Research research={research} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {researchesArray.map(({ id, title }) => (
+          <li
+            className={`text-secondary ${s.hoverEffect}`}
+            key={id}
+            onClick={() => handleClickScroll(id)}
+          >{`# ${title}`}</li>
+        ))}
+      </ul>
+      <ul>
+        {researchesArray.map((research) => (
+          <li
+            className="mb-5 border-2 border-bottom border-danger pb-3"
+            key={research.id}
+          >
+            <Research research={research} />
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
