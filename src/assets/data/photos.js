43c9f96932b8b9518fd976c5bf95b38819f1f78a
Roman Serediuk <box4rom@gmail.com>
2023-04-11 09:56:10 +0300

    feat: create photos data file with draft data

---
+++ src/assets/data/photos.js
@@ -0,0 +1,20 @@
+const photos = [
+  {
+    theme: "theme_1",
+    list: [],
+  },
+  {
+    theme: "theme_2",
+    list: [],
+  },
+  {
+    theme: "theme_3",
+    list: [],
+  },
+  {
+    theme: "theme_4",
+    list: [],
+  },
+];
+
+export default photos;
