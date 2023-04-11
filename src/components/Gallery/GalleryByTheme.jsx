7411832f2d8465a66175eebc1a22eebd087c6fc2
Roman Serediuk <box4rom@gmail.com>
2023-04-11 10:37:51 +0300

    feat: add useEffect to get appropriate list of photos names

--- src/components/Gallery/GalleryByTheme.jsx
+++ src/components/Gallery/GalleryByTheme.jsx
@@ -1,5 +1,20 @@
-const GalleryByTheme = ({ theme }) => {
-  return <div>Gallery By Theme</div>;
+import { useParams, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import galleryThemes from "../../assets/data/galleryThemes.js";
+
+const GalleryByTheme = () => {
+  const { theme } = useParams();
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    const data = galleryThemes.find((item) => item.theme === theme).list;
+    setList(data);
+  }, [theme]);
+  return (
+    <div>
+      <Link to={"/gallery"}>back to gallery</Link>
+      <h4>{theme}</h4>
+    </div>
+  );
 };
 
 export default GalleryByTheme;
