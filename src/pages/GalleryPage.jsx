d251ba974face36f04fc6cf902de6b7c47fd5999
Roman Serediuk <box4rom@gmail.com>
2023-04-11 11:14:50 +0300

    fix: remove useParams, remove props

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -1,12 +1,9 @@
-import { useParams } from "react-router-dom";
 import Gallery from "../components/Gallery/Gallery";
 
 const GalleryPage = () => {
-  const { theme } = useParams();
   return (
     <section className="py-4">
-      <h2>Gallery Page</h2>
-      <Gallery theme={theme} />
+      <Gallery />
     </section>
   );
 };
