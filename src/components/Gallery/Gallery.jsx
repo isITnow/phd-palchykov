d342d6cda552ecc67c4cffe2a77079fa5349bb6f
Roman Serediuk <box4rom@gmail.com>
2023-04-11 13:29:36 +0300

    fix: li className

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -6,7 +6,7 @@ const Gallery = () => {
   return (
     <ul className="row row-cols-1 row-cols-md-2 row-cols-lg-4">
       {galleryThemes.map(({ theme, list }) => (
-        <li key={theme} className={`col mb-3`}>
+        <li key={theme} className="col mb-3">
           <Link to={theme}>
             <div className={`card ${s.hoverEffect}`}>
               <img src="..." className="card-img-top" alt="..." />
