09aa8fd10ce319cba226d5f94243571d8b1391b4
Roman Serediuk <box4rom@gmail.com>
2023-04-11 10:12:27 +0300

    feat: render list of gallery themes, add links to appropriate page

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+import galleryThemes from "../../assets/data/galleryThemes.js";
 // import s from "./gallery.module.css"
 
-const Gallery = ({ theme }) => {
-  return <h4>{theme}</h4>;
+const Gallery = () => {
+  return (
+    <ul>
+      {galleryThemes.map(({ theme, list }) => (
+        <li key={theme}>
+          <Link to={theme}>{theme}</Link>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Gallery;
