9caca71f2243f1354eabf7b3ea044b57e784ca08
Roman Serediuk <box4rom@gmail.com>
2023-04-11 16:38:20 +0300

    feat: create HomePage, implement it in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const ColleaguesPage = lazy(() => import("./pages/ColleaguesPage"));
 const ContactsPage = lazy(() => import("./pages/ContactsPage"));
 const GalleryPage = lazy(() => import("./pages/GalleryPage"));
 const GalleryByThemePage = lazy(() => import("./pages/GalleryByThemePage"));
-// const HomePage = lazy(() => import("./pages/HomePage"));
+const HomePage = lazy(() => import("./pages/HomePage"));
 const NewsPage = lazy(() => import("./pages/NewsPage"));
 const PublicationsPage = lazy(() => import("./pages/PublicationsPage"));
 const ResearchPage = lazy(() => import("./pages/ResearchPage"));
@@ -15,7 +15,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        {/* <Route index element={<HomePage />} /> */}
+        <Route index element={<HomePage />} />
         <Route path="publications/:period" element={<PublicationsPage />} />
         <Route path="research" element={<ResearchPage />} />
         <Route path="gallery" element={<GalleryPage />} />
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import About from "../components/About/About";
+import Sidebar from "../components/Sidebar/Sidebar";
+
+const HomePage = () => {
+  return (
+    <section className="py-4">
+      <div className="row">
+        <div className="col-12 col-md-4 h-25 mb-5">
+          <Sidebar />
+        </div>
+        <div className="col-12 col-md-8">
+          <About />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HomePage;
