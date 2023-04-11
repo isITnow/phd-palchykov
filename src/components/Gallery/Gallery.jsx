e79108e5e84f41ee6b824c8f9daaf333314ecc88
Roman Serediuk <box4rom@gmail.com>
2023-04-11 11:09:54 +0300

    feat: render cards

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom";
 import galleryThemes from "../../assets/data/galleryThemes.js";
-// import s from "./gallery.module.css"
+import s from "./gallery.module.css";
 
 const Gallery = () => {
   return (
-    <ul>
+    <ul className="row row-cols-1 row-cols-md-2 row-cols-lg-4">
       {galleryThemes.map(({ theme, list }) => (
-        <li key={theme}>
-          <Link to={theme}>{theme}</Link>
+        <li key={theme} className={`col mb-3`}>
+          <Link to={theme}>
+            <div className={`card ${s.hoverEffect}`}>
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title text-center">{theme}</h5>
+              </div>
+            </div>
+          </Link>
         </li>
       ))}
     </ul>
