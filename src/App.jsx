7bb612beceb7d05bf8134d8eca49b408bdcdc780
Roman Serediuk <box4rom@gmail.com>
2023-04-11 09:45:47 +0300

    feat: create gallery route, gallery/:theme route, import GalleryByThemePage

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,11 @@ import Layout from "./components/Layout";
 const ColleaguesPage = lazy(() => import("./pages/ColleaguesPage"));
 const ContactsPage = lazy(() => import("./pages/ContactsPage"));
 const GalleryPage = lazy(() => import("./pages/GalleryPage"));
+const GalleryByThemePage = lazy(() => import("./pages/GalleryByThemePage"));
 const HomePage = lazy(() => import("./pages/HomePage"));
 const NewsPage = lazy(() => import("./pages/NewsPage"));
 const PublicationsPage = lazy(() => import("./pages/PublicationsPage"));
 const ResearchPage = lazy(() => import("./pages/ResearchPage"));
-
-// import ColleaguesPage from "./pages/ColleaguesPage";
-// import ContactsPage from "./pages/ContactsPage";
-// import GalleryPage from "./pages/GalleryPage";
-// import HomePage from "./pages/HomePage";
-// import NewsPage from "./pages/NewsPage";
-// import PublicationsPage from "./pages/PublicationsPage";
-// import ResearchPage from "./pages/ResearchPage";
 
 const App = () => {
   return (
@@ -25,7 +18,8 @@ const App = () => {
         <Route index element={<HomePage />} />
         <Route path="publications/:period" element={<PublicationsPage />} />
         <Route path="research" element={<ResearchPage />} />
-        <Route path="gallery/:theme" element={<GalleryPage />} />
+        <Route path="gallery" element={<GalleryPage />} />
+        <Route path="gallery/:theme" element={<GalleryByThemePage />} />
         <Route path="news" element={<NewsPage />} />
         <Route path="colleagues" element={<ColleaguesPage />} />
         <Route path="contacts" element={<ContactsPage />} />
