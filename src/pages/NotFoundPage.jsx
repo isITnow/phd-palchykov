4dad2717beaf3fa91d9ec8bf3d106f0e0b37e6c2
Roman Serediuk <box4rom@gmail.com>
2023-04-12 09:59:45 +0300

    feat: create NotFoundPage

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <span>page not found</span>
+    </div>
+  );
+};
+
+export default NotFoundPage;
