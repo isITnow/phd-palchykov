9ecdf1bf429a257f7ec4511dfa2be6159401856d
Roman Serediuk <box4rom@gmail.com>
2023-04-12 10:34:05 +0300

    feat: add styles

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1>404</h1>
-      <span>page not found</span>
+    <div className="mx-auto my-auto text-center">
+      <h1 className="text-danger fw-bold" style={{ fontSize: "5rem" }}>
+        404
+      </h1>
+      <span className="text-uppercase fw-bold fs-2">page not found</span>
     </div>
   );
 };
