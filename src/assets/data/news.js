1abb0fd632feec96f40e50ed43f64d280a7e86fd
Roman Serediuk <box4rom@gmail.com>
2023-04-14 23:00:38 +0300

    upd: add news data

--- src/assets/data/news.js
+++ src/assets/data/news.js
@@ -3,8 +3,28 @@ const newsArray = [
     id: 1,
     title: "A new specialty at the Oles Honchar Dnipro National University",
     description: "",
+    url: "",
     img: "news_cover-2023-04.jpg",
     date: "April 1, 2023",
+  },
+  {
+    id: 2,
+    title: "We don`t live in a research bubble",
+    description:
+      "Researchers who spoke to The Verge said that science is a global discipline and community, and many institutions have mechanisms in place to promote scientific exchanges and visits. People work in many places throughout their careers and grow to depend on one another. The Ukrainian scientific community has received the resources provided by the individuals, volunteer groups, and institutions with gratitude, although experts say it`s difficult for people to take advantage of the opportunities at this time. Others state that research isn`t a priority right now, given the growing catastrophe in the country. The focus is on fighting the Russians, helping when possible, and staying alive.",
+    url: "https://www.theverge.com/2022/3/17/22979250/ukraine-scientists-international-resources-war",
+    img: "Oleksandr_Pypka_and_Vitalii_Palchykov.jpg",
+    date: "March 17, 2022",
+  },
+  {
+    id: 3,
+    title:
+      "Researchers around the world band together to help fleeing Ukrainian scientists",
+    description:
+      "Until Friday, Dnipro, a city in central-eastern Ukraine, had been relatively safe. But on 11 March, three Russian rockets struck a densely populated civilian area... Looking on in horror the scientific community have come together to offer whatever help they can. Now there is a curated list of research groups from around the world who are offering jobs, scholarships and accommodation. Initiated by Ilfa University of Latvia and supported by national organisations such as the German-Ukrainian Academic Society, this list has grown to include more than 600 universities and has been circulated to all universities in Ukraine. Vitalii Palchykov, a synthetic organic chemist at Dnipro National University, says the list is an `amazing opportunity` for female scientists. Ukrainian men aged 18-60 are not allowed to leave. Palchykov now spends his days trying to write scientific papers, but his lab remains closed. He is also volunteering, doing ‘routine’ work like distributing clothes, medicines and food, and making sandbags and molotov cocktails. He has also helped prepare more than 1000 doses of potassium iodide, to be taken in the first hours of radiation exposure. Looking to the future, he says he fears that Ukrainian science will focus mostly on military projects. ‘I have no doubt that many young scientists will leave the country. This is very sad. I am constantly looking for young scientists for our scientific projects and I understand that it will be more and more difficult.’ In the meantime, he is buoyed by letters from colleagues from all over the world, including Russia.",
+    url: "https://www.chemistryworld.com/news/researchers-around-the-world-band-together-to-help-fleeing-ukrainian-scientists/4015377.article",
+    img: "",
+    date: "March 16, 2022",
   },
 ];
 
