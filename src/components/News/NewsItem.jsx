d19a462857766d1cbf8aca16e92c2d2989d1f294
Roman Serediuk <box4rom@gmail.com>
2023-04-14 23:01:08 +0300

    feat: render link if exist

--- src/components/News/NewsItem.jsx
+++ src/components/News/NewsItem.jsx
@@ -1,12 +1,16 @@
 const NewsItem = ({ news }) => {
-  const { title, description, img, date } = news;
+  const { title, description, img, date, url } = news;
   return (
     <div className="card text-center">
       <div className="card-header">
         <h5 className="card-title text-danger mb-0">{title}</h5>
       </div>
       <div className="card-body">
-        {description && <p className="card-text">{description}</p>}
+        {description && (
+          <p className="card-text" style={{ textAlign: "justify" }}>
+            {description}
+          </p>
+        )}
         {img && (
           <div className="">
             <img
@@ -14,6 +18,18 @@ const NewsItem = ({ news }) => {
               src={require(`../../assets/images/news/${img}`)}
               alt="..."
             />
+          </div>
+        )}
+        {url && (
+          <div className="mt-3">
+            <a
+              className="mb-3"
+              href={url}
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              {url}
+            </a>
           </div>
         )}
       </div>
