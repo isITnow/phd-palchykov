419a5a86f969e783e5699bddd4ce468196ec161d
Roman Serediuk <box4rom@gmail.com>
2023-04-15 17:56:30 +0300

    feat: render list of  alboms

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -4,12 +4,17 @@ import s from "./gallery.module.css";
 
 const Gallery = () => {
   return (
-    <ul className="row row-cols-1 row-cols-md-2 row-cols-lg-4">
-      {galleryThemes.map(({ theme, list }) => (
-        <li key={theme} className="col mb-3">
+    <ul className="row row-cols-1 row-cols-md-2 row-cols-lg-4 align-items-center mb-0">
+      {galleryThemes.map(({ theme, list }, indx) => (
+        <li key={indx} className="col mb-3">
           <Link to={theme}>
             <div className={`card ${s.hoverEffect}`}>
-              <img src="..." className="card-img-top" alt="..." />
+              <img
+                src={require(`../../assets/images/gallery/${theme}/${list[0]}`)}
+                className="card-img-top"
+                alt={`${list[0]}`}
+                style={{ minHeight: "10rem" }}
+              />
               <div className="card-body">
                 <h5 className="card-title text-center">{theme}</h5>
               </div>
