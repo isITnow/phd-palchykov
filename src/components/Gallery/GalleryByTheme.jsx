5eb34362b2b75b5f3296bf6cba01f98746f0d9cc
Roman Serediuk <box4rom@gmail.com>
2023-04-15 17:57:19 +0300

    feat: render actual data by theme

--- src/components/Gallery/GalleryByTheme.jsx
+++ src/components/Gallery/GalleryByTheme.jsx
@@ -11,15 +11,31 @@ const GalleryByTheme = () => {
   }, [theme]);
   return (
     <div>
-      <Link to={"/gallery"}>back to gallery</Link>
-      <h4>{theme}</h4>
-      <ul className="row row-cols-1 row-cols-md-2 row-cols-lg-4">
-        {list.map((img) => (
-          <li key={img} className="col mb-3">
-            <img src="..." alt="..." />
+      <div className="d-flex flex-wrap justify-content-end justify-content-lg-between align-items-center mb-4">
+        <h4 className="text-end text-lg-center text-secondary fw-bold mb-0">
+          {theme}
+        </h4>
+        <Link className="btn btn-link fw-bold" to={"/gallery"}>
+          back to gallery
+        </Link>
+      </div>
+
+      <ul className="row row-cols-1 row-cols-md-2 row-cols-lg-3 align-items-center mb-0">
+        {list.map((img, indx) => (
+          <li key={indx} className="col mb-3">
+            <img
+              src={require(`../../assets/images/gallery/${theme}/${img.trim()}`)}
+              alt={img}
+              className="img-fluid"
+            />
           </li>
         ))}
       </ul>
+      <div className="text-end">
+        <Link className="btn btn-link fw-bold" to={"/gallery"}>
+          back to gallery
+        </Link>
+      </div>
     </div>
   );
 };
