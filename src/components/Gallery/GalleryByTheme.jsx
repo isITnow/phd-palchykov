33f71545089551c6f54677cdd5e57f48d82cabae
Roman Serediuk <box4rom@gmail.com>
2023-04-17 12:49:45 +0300

    feat: create gallery lightbox

--- src/components/Gallery/GalleryByTheme.jsx
+++ src/components/Gallery/GalleryByTheme.jsx
@@ -1,16 +1,20 @@
 import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import galleryThemes from "../../assets/data/galleryThemes.js";
+import { Gallery, Item } from "react-photoswipe-gallery";
+import "photoswipe/dist/photoswipe.css";
 
 const GalleryByTheme = () => {
   const { theme } = useParams();
   const [list, setList] = useState([]);
+
   useEffect(() => {
     const data = galleryThemes.find((item) => item.theme === theme).list;
     setList(data);
   }, [theme]);
+
   return (
-    <div>
+    <>
       <div className="d-flex flex-wrap justify-content-end justify-content-lg-between align-items-center mb-4">
         <h4 className="text-end text-lg-center text-secondary fw-bold mb-0">
           {theme}
@@ -19,24 +23,44 @@ const GalleryByTheme = () => {
           back to gallery
         </Link>
       </div>
-
-      <ul className="row row-cols-1 row-cols-md-2 row-cols-lg-3 align-items-center mb-0">
-        {list.map((img, indx) => (
-          <li key={indx} className="col mb-3">
-            <img
-              src={require(`../../assets/images/gallery/${theme}/${img.trim()}`)}
-              alt={img}
-              className="img-fluid"
-            />
-          </li>
-        ))}
-      </ul>
+      <Gallery withCaption>
+        <div className="row row-cols-2 row-cols-lg-4 align-items-center mb-0">
+          {list.map((item, indx) => {
+            const {
+              filename,
+              size: { width, hight },
+              caption,
+            } = item;
+            return (
+              <div key={indx} className="col mb-3">
+                <Item
+                  original={require(`../../assets/images/gallery/${theme}/${filename.trim()}`)}
+                  thumbnail={require(`../../assets/images/gallery/${theme}/${filename.trim()}`)}
+                  width={width}
+                  height={hight}
+                  caption={caption ? caption : null}
+                >
+                  {({ ref, open }) => (
+                    <img
+                      ref={ref}
+                      onClick={open}
+                      src={require(`../../assets/images/gallery/${theme}/${filename.trim()}`)}
+                      alt={filename}
+                      className="img-fluid"
+                    />
+                  )}
+                </Item>
+              </div>
+            );
+          })}
+        </div>
+      </Gallery>
       <div className="text-end">
         <Link className="btn btn-link fw-bold" to={"/gallery"}>
           back to gallery
         </Link>
       </div>
-    </div>
+    </>
   );
 };
 
