3c764564219fd3672cf3637f9b22cbdd3e5d3d02
Roman Serediuk <box4rom@gmail.com>
2023-04-27 10:40:25 +0300

    fix: ul margin depending on the margin prop

--- src/components/PagesNav/PagesNav.jsx
+++ src/components/PagesNav/PagesNav.jsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 import s from "./pagesNav.module.css";
 
-const PagesNav = ({ location, list }) => {
+const PagesNav = ({ location, list, margin = "my-0" }) => {
   const isDisabled = (name) => (location === name ? "none" : "auto");
   const getStyle = (name) =>
     location === name
       ? "mb-3 text-danger fw-bold"
       : `mb-3 text-secondary fw-bold ${s.hoverEffect}`;
   return (
-    <ul className="mb-0 text-end">
+    <ul className={`text-end ${margin}`}>
       {list.map((name, indx) => (
         <li key={indx}>
           <Link
