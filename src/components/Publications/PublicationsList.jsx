b592cc438034167823150a199b2570d1ada356d1
Roman Serediuk <box4rom@gmail.com>
2023-04-27 10:42:40 +0300

    fix: ul mb-0

--- src/components/Publications/PublicationsList.jsx
+++ src/components/Publications/PublicationsList.jsx
@@ -2,7 +2,7 @@ import Publication from "./Publication";
 
 const PublicationsList = ({ publications }) => {
   return (
-    <ul className="row row-cols-1 row-cols-md-2 g-2">
+    <ul className="row row-cols-1 row-cols-md-2 g-2 mb-0">
       {publications.map((publication) => (
         <li key={publication.id} className="col">
           <Publication publication={publication} />
