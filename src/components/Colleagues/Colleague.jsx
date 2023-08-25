2af108ae7372efb4f2c0a03cccd319069df7b026
Roman Serediuk <box4rom@gmail.com>
2023-08-25 16:10:30 +0300

    feat: create components and perform logic to provide colleagues CRUD operations

--- src/components/Colleagues/Colleague.jsx
+++ src/components/Colleagues/Colleague.jsx
@@ -1,22 +1,25 @@
+import { Link } from "react-router-dom";
 import s from "./colleague.module.css";
 
-const Colleague = ({ colleague }) => {
-  const { name, position, photo, phone, email } = colleague;
+const Colleague = ({ colleague, onDelete, btnDisable }) => {
+  const { id, name, position, photo_url, phone, email } = colleague;
+
+  const handleClick = (id) => {
+    alert("Are you sure you want to delete item?");
+    onDelete(id);
+  };
+
   return (
     <div className="card h-100 overflow-hidden shadow-sm">
       <div className="row g-0 h-100">
         <div className="col-4">
-          <img
-            src={require(`../../assets/images/colleagues/${photo}`)}
-            className={s.img}
-            alt={name}
-          />
+          <img src={photo_url} className={s.img} alt={name} />
         </div>
         <div className="col-8">
           <div className="card-body">
             <h5 className="card-title">{name}</h5>
             <p className="card-text">{position}</p>
-            <ul>
+            <ul className="mb-3">
               <li className="mb-2">
                 <a className="py-2" href={`mailto:${email}`}>
                   {email}
@@ -28,6 +31,23 @@ const Colleague = ({ colleague }) => {
                 </a>
               </li>
             </ul>
+            {/* TODO: fix btn-group position */}
+            <div className="btn-group position-absolute bottom-0 end-0">
+              <Link
+                className="btn btn-sm btn-primary"
+                to={`/colleagues/${id}/edit`}
+                state={colleague}
+              >
+                edit
+              </Link>
+              <button
+                disabled={btnDisable}
+                onClick={() => handleClick(id)}
+                className="btn btn-sm btn-danger"
+              >
+                delete
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Colleagues/ColleaguesList.jsx
+++ src/components/Colleagues/ColleaguesList.jsx
@@ -1,15 +1,77 @@
+import { useEffect, useState } from "react";
+import { useAlert } from "../../assets/utils/useAlert.js";
+
+import Alert from "../Alert.jsx";
 import Colleague from "./Colleague";
-import colleaguesArray from "../../assets/data/colleagues";
+import Loader from "../Loader";
+
+import { colleaguesAPI } from "../../services/colleaguesAPI";
+
+let message = "";
 
 const ColleaguesList = () => {
+  const [colleaguesList, setColleaguesList] = useState([]);
+  const [isReady, setIsReady] = useState(false);
+  const [toggle, setToggle] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const { alert, showAlert } = useAlert();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await colleaguesAPI.fetchColleagues();
+        setColleaguesList(data);
+        setIsReady(true);
+      } catch (error) {
+        message = "Error occurred. Contact your administrator!";
+        showAlert(message, "danger");
+
+        console.log("error: ", error);
+      }
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [toggle]);
+
+  const deleteHandler = async (id) => {
+    setIsDisabled(true);
+    try {
+      const response = await colleaguesAPI.deleteColleague(id);
+      if (response.status === 204) {
+        console.log("DELETE SUCCESS");
+        setToggle((prevState) => !prevState);
+      } else {
+        message = "Failed to delete record. Contact your administrator!";
+        showAlert(message, "danger");
+      }
+    } catch (error) {
+      message = "Error occurred. Contact your administrator!";
+      showAlert(message, "danger");
+
+      console.log("Error occurred while deleting the record:", error);
+    } finally {
+      setIsDisabled(false);
+    }
+  };
+
+  if (!isReady) {
+    return <Loader />;
+  }
+
   return (
-    <ul className="row row-cols-1 row-cols-lg-2">
-      {colleaguesArray.map((colleague) => (
-        <li className="col mb-3" key={colleague.id}>
-          <Colleague colleague={colleague} />
-        </li>
-      ))}
-    </ul>
+    <>
+      {alert.visible && <Alert state={alert} />}
+      <ul className="row row-cols-1 row-cols-lg-2">
+        {colleaguesList.map((colleague) => (
+          <li className="col mb-3" key={colleague.id}>
+            <Colleague
+              onDelete={deleteHandler}
+              btnDisable={isDisabled}
+              colleague={colleague}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
