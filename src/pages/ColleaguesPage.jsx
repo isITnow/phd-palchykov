23ef54a7011ac8c78b372c26c75eb8f52f6c9bb5
Roman Serediuk <box4rom@gmail.com>
2023-08-25 13:17:05 +0300

    feat: add link btn to /colleagues/new

--- src/pages/ColleaguesPage.jsx
+++ src/pages/ColleaguesPage.jsx
@@ -1,5 +1,6 @@
-import ColleaguesList from "../components/Colleagues/ColleaguesList";
+import { Link } from "react-router-dom";
 
+import ColleaguesList from "../components/Colleagues/ColleaguesList";
 import setPageTitle from "../assets/utils/setPageTitle";
 
 const ColleaguesPage = () => {
@@ -7,6 +8,11 @@ const ColleaguesPage = () => {
   return (
     <section className="py-4">
       <ColleaguesList />
+      <div className="mt-3">
+        <Link className="btn btn-primary" to={"/colleagues/new"}>
+          new colleague
+        </Link>
+      </div>
     </section>
   );
 };
