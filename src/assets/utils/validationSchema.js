bb7f0050839e5873ee3b3506f5374fc561c18802
Roman Serediuk <box4rom@gmail.com>
2023-08-25 16:03:58 +0300

    feat: add colleagueSchema

--- src/assets/utils/validationSchema.js
+++ src/assets/utils/validationSchema.js
@@ -1,12 +1,20 @@
 import * as yup from "yup";
 
 const newsSchema = yup.object().shape({
-  title: yup.string().min(2, "Too short").required(true, "Required"),
+  title: yup.string().min(2, "Too short").required("Required"),
   body: yup.string().min(2, "Too short"),
   date: yup.string().required("Required"),
   link: yup.string(),
 });
 
+const colleagueSchema = yup.object().shape({
+  name: yup.string().min(2, "Too short").required("Required"),
+  position: yup.string().min(2, "Too short").required("Required"),
+  phone: yup.string(),
+  email: yup.string(),
+});
+
 export const validation = {
   newsSchema,
+  colleagueSchema,
 };
