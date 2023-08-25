6e6ad19f19899daff2c5939e7620092fa1dacde7
Roman Serediuk <box4rom@gmail.com>
2023-08-25 13:14:04 +0300

    create colleaguesAPI

---
+++ src/services/colleaguesAPI.js
@@ -0,0 +1,34 @@
+import { api } from "./http";
+
+const fetchColleagues = async () => {
+  const data = await api.get("/colleagues");
+
+  return data;
+};
+
+const postColleague = async (body) => {
+  console.log("colleaguesAPI/request body: ", body);
+
+  const data = await api.post("/colleagues", body, {});
+
+  return data;
+};
+
+const deleteColleague = async (id) => {
+  const data = await api.delete(`/colleagues/${id}`, id);
+
+  return data;
+};
+
+const editColleague = async (id, body) => {
+  const data = await api.patch(`/colleagues/${id}`, body);
+
+  return data;
+};
+
+export const newsAPI = {
+  fetchColleagues,
+  postColleague,
+  deleteColleague,
+  editColleague,
+};
