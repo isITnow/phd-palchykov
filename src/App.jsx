d31c3aa616d7de5c5ad61fcd0e2d920575abffd4
Roman Serediuk <box4rom@gmail.com>
2023-08-25 16:42:44 +0300

    upd: pages load order

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { Routes, Route } from "react-router-dom";
 import { lazy } from "react";
 import Layout from "./components/Layout";
 
-const NewColleaguePage = lazy(() => import("./pages/NewColleaguePage"));
-const EditColleaguePage = lazy(() => import("./pages/EditColleaguePage"));
+const NewsPage = lazy(() => import("./pages/NewsPage"));
+const EditNewsPage = lazy(() => import("./pages/EditNewsPage"));
+const NewNewsPage = lazy(() => import("./pages/NewNewsPage"));
+
 const ColleaguesPage = lazy(() => import("./pages/ColleaguesPage"));
-const ContactsPage = lazy(() => import("./pages/ContactsPage"));
+const EditColleaguePage = lazy(() => import("./pages/EditColleaguePage"));
+const NewColleaguePage = lazy(() => import("./pages/NewColleaguePage"));
+
 const GalleryPage = lazy(() => import("./pages/GalleryPage"));
 const GalleryByThemePage = lazy(() => import("./pages/GalleryByThemePage"));
+
+const ContactsPage = lazy(() => import("./pages/ContactsPage"));
 const HomePage = lazy(() => import("./pages/HomePage"));
-const NewsPage = lazy(() => import("./pages/NewsPage"));
-const NewNewsPage = lazy(() => import("./pages/NewNewsPage"));
-const EditNewsPage = lazy(() => import("./pages/EditNewsPage"));
 const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
 const PublicationsPage = lazy(() => import("./pages/PublicationsPage"));
 const ResearchPage = lazy(() => import("./pages/ResearchPage"));
