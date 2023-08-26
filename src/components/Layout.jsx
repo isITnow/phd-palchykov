f179e01bea6807ebebde7374abc3bb406fcdc8ab
Roman Serediuk <box4rom@gmail.com>
2023-08-26 15:02:46 +0300

    feat: implement fetch all publication periods

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,20 +1,35 @@
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer/Footer";
 import Loader from "../components/Loader";
 import Navigation from "./Navigation/Navigation";
 
+import { periodsAPI } from "../services/publicationPeriodsAPI";
+
 const Layout = () => {
+  const [periods, setPeriods] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await periodsAPI.fetchPeriods();
+        setPeriods(data);
+      } catch (error) {
+        console.log("Periods error", error);
+      }
+    })();
+  }, []);
+
   return (
     <>
       <header className="shadow-sm py-3">
         <div className="container">
-          <Navigation />
+          <Navigation periods={periods} />
         </div>
       </header>
       <main className="container">
         <Suspense fallback={<Loader />}>
-          <Outlet />
+          <Outlet context={periods} />
         </Suspense>
       </main>
       <Footer />
