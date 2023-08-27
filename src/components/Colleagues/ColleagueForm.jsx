2ab9fbf664eddef63ba17ad09bfa1fdb9b3b8ea1
Roman Serediuk <box4rom@gmail.com>
2023-08-27 11:42:53 +0300

    feat: implement redux asyncThunc to post colleague

--- src/components/Colleagues/ColleagueForm.jsx
+++ src/components/Colleagues/ColleagueForm.jsx
@@ -2,20 +2,25 @@ import { Form, Formik } from "formik";
 import { useNavigate } from "react-router-dom";
 import { useAlert } from "../../assets/utils/useAlert";
 
+import { useDispatch, useSelector } from "react-redux";
+import { addColleagueThunk } from "../../redux/colleagues/operationsColleagues";
+import { selectColleagues } from "../../redux/colleagues/selectorColleagues";
+
 import Alert from "../Alert";
 import CustomInput from "../FormComponents/CustomInput";
 
-import { colleaguesAPI } from "../../services/colleaguesAPI";
 import { validation } from "../../assets/utils/validationSchema";
 
 const ColleagueForm = ({ colleague }) => {
+  const dispatch = useDispatch();
+  const { status, error } = useSelector(selectColleagues);
+
   const isNewItem = !colleague;
   const navigate = useNavigate();
   const { alert, showAlert } = useAlert();
 
   const handleSubmit = async (values, actions) => {
     const { name, position, phone, email, photo } = values;
-    let message = "";
 
     const formData = new FormData();
     formData.append("colleague[name]", name);
@@ -26,29 +31,16 @@ const ColleagueForm = ({ colleague }) => {
     if (photo) {
       formData.append("colleague[photo]", photo);
     }
+    console.log("form error: ", error);
+    console.log("form status: ", status);
 
-    try {
-      const response = isNewItem
-        ? await colleaguesAPI.postColleague(formData)
-        : await colleaguesAPI.editColleague(colleague.id, formData);
+    dispatch(addColleagueThunk(formData));
 
-      if (response.status === 201) {
-        actions.resetForm();
-        message = "Card created successfully";
-        showAlert(message, "success");
-      } else if (response.status === 202) {
-        message = "Card updated successfully";
-        showAlert(message, "success");
-        return navigate("/colleagues");
-      } else {
-        message = "Failed to save record. Contact your administrator!";
-        showAlert(message, "danger");
-      }
-    } catch (error) {
-      message = "Error occurred. Contact your administrator!";
-      showAlert(message, "danger");
-
-      console.log("Error occurred while saving the record:", error);
+    if (status === "fulfilled") {
+      actions.resetForm();
+      showAlert("Card created successfully", "success");
+    } else if (status === "rejected") {
+      showAlert(error, "danger");
     }
   };
 
@@ -84,7 +76,7 @@ const ColleagueForm = ({ colleague }) => {
               }}
             />
             <button
-              disabled={props.isSubmitting}
+              disabled={status === "pending"}
               type="submit"
               className="btn btn-primary mt-3"
             >
