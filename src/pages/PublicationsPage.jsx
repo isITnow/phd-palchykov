ad7c92d7d07fde83bf9aa84c218abcb5dccdfa5d
Roman Serediuk <box4rom@gmail.com>
2023-08-28 21:00:20 +0300

    feat: implement redux asyncSunc to get all publications

--- src/pages/PublicationsPage.jsx
+++ src/pages/PublicationsPage.jsx
@@ -1,56 +1,46 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import PagesNav from "../components/PagesNav/PagesNav";
-import PublicationsList from "../components/Publications/PublicationsList";
+import { useEffect } from "react";
 
-import { publicationsAPI } from "../services/publicationsAPI";
-import { useOutletContext } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getPublicationsThunk } from "../redux/publications/operationsPublications";
+import { selectPublications } from "../redux/publications/selectorPublications";
+import { selectPeriods } from "../redux/publicationPeriods/selectorPublicationPeriods";
+
+import PagesNav from "../components/PagesNav/PagesNav";
+import Loader from "../components/Loader";
+import PublicationsList from "../components/Publications/PublicationsList";
 
 import setPageTitle from "../assets/utils/setPageTitle";
 
 const PublicationsPage = () => {
   setPageTitle("Publications");
   const { id } = useParams();
-  console.log("id: ", id);
-  const periods = useOutletContext();
-  console.log("periods: ", periods);
-  const [publicationsList, setPublicationsList] = useState([]);
-
-  const currentPeriod = periods.find((period) => period.id === id);
-  console.log("currentPeriod: ", currentPeriod);
-  // const periodsList = publicationsArray.map(
-  //   (publication) => publication.period
-  // );
+  const dispatch = useDispatch();
+  const { periods } = useSelector(selectPeriods);
+  const { publications, status, error } = useSelector(selectPublications);
+  // console.log("publications: ", publications);
 
   useEffect(() => {
-    (async () => {
-      try {
-        const { data } = await publicationsAPI.fetchPublications(id);
-        console.log("data: ", data);
-        // setPeriods(data);
-      } catch (error) {
-        console.log("Periods error", error);
-      }
-    })();
-  }, [id]);
+    dispatch(getPublicationsThunk(id));
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [dispatch, id]);
 
-  // useEffect(() => {
-  //   const data = publicationsArray.find(
-  //     (publication) => publication.period === period
-  //   ).publicationsList;
-  //   setList(data);
-  //   window.scrollTo({
-  //     top: 0,
-  //     behavior: "smooth",
-  //   });
-  // }, [period]);
+  if (status === "loading") {
+    return <Loader />;
+  }
 
   return (
     <section className="py-4">
-      {/* <p className="mb-3 text-secondary text-end fw-bold">{}</p> */}
-      {/* <PagesNav margin={"mb-3"} list={periodsList} location={period} /> */}
-      <PublicationsList publications={publicationsList} />
-      {/* <PagesNav margin={"mt-3 mb-0"} list={periodsList} location={period} /> */}
+      <PagesNav
+        margin={"mb-3"}
+        periods={periods}
+        currentPeriodId={parseInt(id)}
+      />
+      <PublicationsList publications={publications} />
+      <PagesNav margin={"mt-3 mb-0"} periods={periods} currentPeriodId={id} />
     </section>
   );
 };
