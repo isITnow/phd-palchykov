2d19d412bfaab0ef490b87ba3f3173b2adf44e0a
Roman Serediuk <box4rom@gmail.com>
2023-08-28 21:05:22 +0300

    upd: temporary comment some  code lines

--- src/components/Publications/Publication.jsx
+++ src/components/Publications/Publication.jsx
@@ -1,7 +1,7 @@
 import s from "./publication.module.css";
 
 const Publication = ({ publication }) => {
-  const { id, title, authors, source, url, cover_img, abstract_img } =
+  const { id, title, authors, source, source_url, cover_url, abstract_url } =
     publication;
   return (
     <div className={`card h-100 ${s.hoverEffect}`}>
@@ -10,18 +10,18 @@ const Publication = ({ publication }) => {
           <span className="me-2 text-secondary fs-5 lh-sm">{id}.</span>
           <h5 className="card-title text-danger">{title}</h5>
         </div>
-        {cover_img && abstract_img ? (
+        {/* {cover_url && abstract_url ? (
           <div className="row row-cols-2 mt-2">
             <div className="col">
               <img
-                src={require(`../../assets/images/publications/${cover_img}`)}
+                src={require(`../../assets/images/publications/${cover_url}`)}
                 className={`shadow rounded ${s.img}`}
-                alt={cover_img}
+                alt={cover_url}
               />
             </div>
             <div className="col">
               <p className="card-subtitle mt-2">{authors.join("; ")}</p>
-              <a href={url} target="_blank" rel="noreferrer noopener">
+              <a href={source_url} target="_blank" rel="noreferrer noopener">
                 <p className="fst-italic mt-2">{source}</p>
               </a>
             </div>
@@ -29,20 +29,20 @@ const Publication = ({ publication }) => {
         ) : (
           <div>
             <p className="card-subtitle mt-2">{authors.join("; ")}</p>
-            <a href={url} target="_blank" rel="noreferrer noopener">
+            <a href={source_url} target="_blank" rel="noreferrer noopener">
               <p className="fst-italic mt-2">{source}</p>
             </a>
           </div>
-        )}
-        <div className="mt-3">
+        )} */}
+        {/* <div className="mt-3">
           <img
             src={require(`../../assets/images/publications/${
-              abstract_img || cover_img
+              abstract_url || cover_url
             }`)}
             className={`shadow  rounded ${s.img}`}
-            alt={abstract_img || cover_img}
+            alt={abstract_url || cover_url}
           />
-        </div>
+        </div> */}
       </div>
     </div>
   );
