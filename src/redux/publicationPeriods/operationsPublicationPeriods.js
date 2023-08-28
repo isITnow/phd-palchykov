2329bf27cb9370e6caae6391b0065d19a6d68090
Roman Serediuk <box4rom@gmail.com>
2023-08-28 19:16:34 +0300

    create PublicationPeriod slice, operations, selector

---
+++ src/redux/publicationPeriods/operationsPublicationPeriods.js
@@ -0,0 +1,75 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { periodsAPI } from "../../services/publicationPeriodsAPI";
+
+export const getPeriodsThunk = createAsyncThunk(
+  "periods/get",
+
+  async (_, { rejectWithValue }) => {
+    try {
+      const resp = await periodsAPI.fetchPeriods();
+
+      if (resp.status !== 200) {
+        throw new Error("Error occurred! Please contact your administrator.");
+      }
+      return resp.data;
+    } catch (error) {
+      console.log("GET news error: ", error);
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+// export const addPeriodsThunk = createAsyncThunk(
+//   "periods/post",
+
+//   async (news, { rejectWithValue }) => {
+//     try {
+//       const resp = await periodsAPI.postNews(news);
+
+//       if (resp.status !== 201) {
+//         throw new Error("Error occurred! Please contact your administrator.");
+//       }
+//       return resp.data;
+//     } catch (error) {
+//       console.log("POST news error: ", error);
+//       return rejectWithValue(error.message);
+//     }
+//   }
+// );
+
+// export const updatePeriodsThunk = createAsyncThunk(
+//   "periods/update",
+
+//   async ({ id, news }, { dispatch, rejectWithValue }) => {
+//     try {
+//       const resp = await periodsAPI.editNews(id, news);
+
+//       if (resp.status !== 202) {
+//         throw new Error("Error occurred! Please contact your administrator.");
+//       }
+//       dispatch(getPeriodsThunk());
+//       return resp.data;
+//     } catch (error) {
+//       console.log("EDIT news error: ", error);
+//       return rejectWithValue(error.message);
+//     }
+//   }
+// );
+
+// export const removePeriodsThunk = createAsyncThunk(
+//   "periods/delete",
+
+//   async (id, { dispatch, rejectWithValue }) => {
+//     try {
+//       const resp = await periodsAPI.deleteNews(id);
+
+//       if (resp.status !== 204) {
+//         throw new Error("Error occurred! Please contact your administrator.");
+//       }
+//       return id;
+//     } catch (error) {
+//       console.log("DELETE news error: ", error);
+//       return rejectWithValue(error.message);
+//     }
+//   }
+// );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import colleaguesReducer from "./colleagues/sliceColleagues";
 import newsReducer from "./news/sliceNews";
+import publicationPeriodsReducer from "./publicationPeriods/slicePublicationPeriods";
 
 export default configureStore({
   reducer: {
     colleagues: colleaguesReducer,
     news: newsReducer,
+    periods: publicationPeriodsReducer,
   },
 });
