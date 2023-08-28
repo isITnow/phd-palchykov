bfdefff8d137f04a3219a26a56cce91fc0b55d22
Roman Serediuk <box4rom@gmail.com>
2023-08-28 17:50:11 +0300

    feat: implement redux asyncThunc; remove alert messages

--- src/components/News/NewsForm.jsx
+++ src/components/News/NewsForm.jsx
@@ -1,160 +1,135 @@
 import { FieldArray, Form, Formik } from "formik";
-import { useNavigate } from "react-router-dom";
-import { useAlert } from "../../assets/utils/useAlert";
 
-import Alert from "../Alert";
+import { useDispatch, useSelector } from "react-redux";
+import { addNewsThunk, updateNewsThunk } from "../../redux/news/operationsNews";
+import { selectNews } from "../../redux/news/selectorNews";
+
 import CustomInput from "../FormComponents/CustomInput";
 import CustomTextArea from "../FormComponents/CustomTextArea";
 
-import { newsAPI } from "../../services/newsAPI";
 import { validation } from "../../assets/utils/validationSchema";
 
 const NewsForm = ({ newsItem }) => {
+  const dispatch = useDispatch();
+  const { status } = useSelector(selectNews);
   const isNewItem = !newsItem;
-  const navigate = useNavigate();
-  const { alert, showAlert } = useAlert();
 
   const handleSubmit = async (values, actions) => {
     const { title, body, date, image, links } = values;
-    let message = "";
 
     const formData = new FormData();
-    formData.append("news[title]", title);
-    formData.append("news[body]", body);
-    formData.append("news[date]", date);
+    formData.append("news[title]", title.trim());
+    formData.append("news[body]", body.trim());
+    formData.append("news[date]", date.trim());
     if (links.length) {
       links.forEach((element) => {
-        formData.append("news[links][]", element);
+        formData.append("news[links][]", element.trim());
       });
     }
     if (image) {
       formData.append("news[image]", image);
     }
 
-    try {
-      const response = isNewItem
-        ? await newsAPI.postNews(formData)
-        : await newsAPI.editNews(newsItem.id, formData);
+    isNewItem
+      ? dispatch(addNewsThunk(formData))
+      : dispatch(updateNewsThunk({ id: newsItem.id, news: formData }));
 
-      if (response.status === 201) {
-        actions.resetForm();
-        message = "News created successfully";
-        showAlert(message, "success");
-      } else if (response.status === 202) {
-        message = "News updated successfully";
-        showAlert(message, "success");
-        return navigate("/news");
-      } else {
-        message = "Failed to save record. Contact your administrator!";
-        showAlert(message, "danger");
-      }
-    } catch (error) {
-      message = "Error occurred. Contact your administrator!";
-      showAlert(message, "danger");
-
-      console.log("Error occurred while saving the record:", error);
+    if (status === "fulfilled" && isNewItem) {
+      actions.resetForm();
     }
   };
 
   return (
-    <>
-      {alert.visible && <Alert state={alert} />}
-      <Formik
-        initialValues={
-          isNewItem
-            ? {
-                title: "",
-                body: "",
-                date: "",
-                links: [],
-                image: "",
-              }
-            : newsItem
-        }
-        validationSchema={validation.newsSchema}
-        onSubmit={handleSubmit}
-      >
-        {(props) => (
-          <Form>
-            <CustomInput
-              label="News title"
-              name="title"
-              type="text"
-              autoFocus
-            />
-            <CustomTextArea
-              label="Description"
-              name="body"
-              type="text-area"
-              rows="5"
-            />
-            <CustomInput label="Date" name="date" type="text" />
-            <input
-              className="form-control mb-3"
-              type="file"
-              onChange={(e) => {
-                props.setFieldValue("image", e.target.files[0]);
-              }}
-            />
-            <div>
-              <FieldArray name="links">
-                {(fieldArrayProps) => {
-                  const { push, remove, insert, form } = fieldArrayProps;
-                  const { values } = form;
-                  const { links } = values;
-                  return (
-                    <div>
-                      {links && links.length > 0 ? (
-                        links.map((link, index) => (
-                          <div key={index}>
-                            <CustomInput
-                              type="text"
-                              label="Link"
-                              name={`links.${index}`}
-                            />
-                            <div className="btn-group mb-3" role="group">
-                              <button
-                                type="button"
-                                className="btn btn-outline-primary"
-                                onClick={() => remove(index)} // remove a friend from the list
-                              >
-                                remove link
-                              </button>
-                              <button
-                                type="button"
-                                className="btn btn-outline-primary"
-                                onClick={() => insert(index, "")} // insert an empty string at a position
-                              >
-                                add a new link
-                              </button>
-                            </div>
+    <Formik
+      initialValues={
+        isNewItem
+          ? {
+              title: "",
+              body: "",
+              date: "",
+              links: [],
+              image: "",
+            }
+          : newsItem
+      }
+      validationSchema={validation.newsSchema}
+      onSubmit={handleSubmit}
+    >
+      {(props) => (
+        <Form>
+          <CustomInput label="News title" name="title" type="text" autoFocus />
+          <CustomTextArea
+            label="Description"
+            name="body"
+            type="text-area"
+            rows="5"
+          />
+          <CustomInput label="Date" name="date" type="text" />
+          <input
+            className="form-control mb-3"
+            type="file"
+            onChange={(e) => {
+              props.setFieldValue("image", e.target.files[0]);
+            }}
+          />
+          <div>
+            <FieldArray name="links">
+              {(fieldArrayProps) => {
+                const { push, remove, insert, form } = fieldArrayProps;
+                const { values } = form;
+                const { links } = values;
+                return (
+                  <div>
+                    {links && links.length > 0 ? (
+                      links.map((link, index) => (
+                        <div key={index}>
+                          <CustomInput
+                            type="text"
+                            label="Link"
+                            name={`links.${index}`}
+                          />
+                          <div className="btn-group mb-3" role="group">
+                            <button
+                              type="button"
+                              className="btn btn-outline-primary"
+                              onClick={() => remove(index)} // remove a friend from the list
+                            >
+                              remove link
+                            </button>
+                            <button
+                              type="button"
+                              className="btn btn-outline-primary"
+                              onClick={() => insert(index, "")} // insert an empty string at a position
+                            >
+                              add a new link
+                            </button>
                           </div>
-                        ))
-                      ) : (
-                        <button
-                          type="button"
-                          className="btn btn-outline-primary"
-                          onClick={() => push("")}
-                        >
-                          Add a link
-                        </button>
-                      )}
-                    </div>
-                  );
-                }}
-              </FieldArray>
-            </div>
-            <button
-              disabled={props.isSubmitting}
-              type="submit"
-              className="btn btn-primary mt-3"
-            >
-              {isNewItem ? "Create news" : "Update news"}
-            </button>
-          </Form>
-        )}
-      </Formik>
-    </>
+                        </div>
+                      ))
+                    ) : (
+                      <button
+                        type="button"
+                        className="btn btn-outline-primary"
+                        onClick={() => push("")}
+                      >
+                        Add a link
+                      </button>
+                    )}
+                  </div>
+                );
+              }}
+            </FieldArray>
+          </div>
+          <button
+            disabled={props.isSubmitting}
+            type="submit"
+            className="btn btn-primary mt-3"
+          >
+            {isNewItem ? "Create news" : "Update news"}
+          </button>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
