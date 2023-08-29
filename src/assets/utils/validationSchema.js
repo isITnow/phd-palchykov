8d7be8e6c8e882862213238bd8c56d3844c04029
Roman Serediuk <box4rom@gmail.com>
2023-08-29 10:00:04 +0300

    feat: add publicationSchema

--- src/assets/utils/validationSchema.js
+++ src/assets/utils/validationSchema.js
@@ -1,20 +1,28 @@
 import * as yup from "yup";
 
 const newsSchema = yup.object().shape({
-  title: yup.string().min(2, "Too short").required("Required"),
-  body: yup.string().min(2, "Too short"),
+  title: yup.string().min(5, "Too short").required("Required"),
+  body: yup.string().min(10, "Too short"),
   date: yup.string().required("Required"),
   link: yup.string(),
 });
 
 const colleagueSchema = yup.object().shape({
-  name: yup.string().min(2, "Too short").required("Required"),
-  position: yup.string().min(2, "Too short").required("Required"),
+  name: yup.string().min(5, "Too short").required("Required"),
+  position: yup.string().min(5, "Too short").required("Required"),
   phone: yup.string(),
   email: yup.string(),
+});
+
+const publicationSchema = yup.object().shape({
+  title: yup.string().min(5, "Too short").required("Required"),
+  source: yup.string().min(5, "Too short").required("Required"),
+  source_url: yup.string().min(5, "Too short").required("Required"),
+  // author: yup.string().min(5, "Too short").required("Required"),
 });
 
 export const validation = {
   newsSchema,
   colleagueSchema,
+  publicationSchema,
 };
