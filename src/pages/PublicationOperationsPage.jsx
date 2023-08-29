6c8f2771bfaa1b3b9ecd7535506def5306ad678c
Roman Serediuk <box4rom@gmail.com>
2023-08-29 10:05:20 +0300

    upd: update page title; implement PublicationForm

--- src/pages/PublicationOperationsPage.jsx
+++ src/pages/PublicationOperationsPage.jsx
@@ -3,21 +3,28 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectPublications } from "../redux/publications/selectorPublications";
+import { selectPeriods } from "../redux/publicationPeriods/selectorPublicationPeriods";
 
-// import PublicationForm from "../components/Publications/PublicationForm";
+import PublicationForm from "../components/Publications/PublicationForm";
 import Alert from "../components/Alert";
 import { useAlert } from "../assets/utils/useAlert";
 
 const PublicationOperationsPage = ({ edit }) => {
   const { publications, error, status } = useSelector(selectPublications);
+  const { periods } = useSelector(selectPeriods);
   const { alert, showAlert } = useAlert();
-  const { id } = useParams();
-  const title = edit ? "Edit publication" : "Create publication";
+  const { period_id } = useParams();
+  const currentPeriodId = parseInt(period_id);
+  const currentPeriod = periods.find((period) => period.id === currentPeriodId);
+
+  const title = edit
+    ? `Edit publication [period: ${currentPeriod.title}]`
+    : `Create publication [period: ${currentPeriod.title}]`;
   let publication = null;
 
   if (edit) {
     publication = publications.find(
-      (publication) => publication.id === parseInt(id)
+      (publication) => publication.id === currentPeriodId
     );
   }
 
@@ -53,7 +60,7 @@ const PublicationOperationsPage = ({ edit }) => {
             </li>
           </ul>
         </div>
-        {/* <PublicationForm colleague={edit ? publication : null} /> */}
+        <PublicationForm colleague={edit ? publication : null} />
       </section>
     </>
   );
