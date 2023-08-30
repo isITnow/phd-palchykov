9140d14f2bf6d6c6ac2d8d818921a40e188f4509
Roman Serediuk <box4rom@gmail.com>
2023-08-30 21:16:47 +0300

    feat: implement redux asuncThunk to provide get researches and show alert

--- src/pages/ResearchPage.jsx
+++ src/pages/ResearchPage.jsx
@@ -1,13 +1,51 @@
-import ResearchList from "../components/Research/ResearchList";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
+import { useDispatch, useSelector } from "react-redux";
+import { selectResearches } from "../redux/researches/selectorResearches";
+import { getResearchesThunk } from "../redux/researches/operationsResearches";
+
+import Alert from "../components/Alert";
+import { useAlert } from "../assets/utils/useAlert";
+
+import ResearchList from "../components/Research/ResearchList";
 import setPageTitle from "../assets/utils/setPageTitle";
+import Loader from "../components/Loader";
 
 const ResearchPage = () => {
   setPageTitle("Research");
+  const dispatch = useDispatch();
+  const { researches, status, error } = useSelector(selectResearches);
+  const { alert, showAlert } = useAlert();
+
+  useEffect(() => {
+    dispatch(getResearchesThunk());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (status === "rejected") {
+      showAlert(`${error}. Please contact your administrator!`, "danger");
+      return;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [status]);
+
+  if (status === "loading") {
+    return <Loader />;
+  }
+
   return (
-    <section className="py-4">
-      <ResearchList />
-    </section>
+    <>
+      {alert.visible && <Alert state={alert} />}
+      <section className="py-4">
+        <ResearchList researches={researches} />
+        <div className="">
+          <Link className="btn btn-primary" to={"/research/new"}>
+            new research
+          </Link>
+        </div>
+      </section>
+    </>
   );
 };
 
