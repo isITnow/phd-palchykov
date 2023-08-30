066c25a618cd9f0312fe04708dc087248a853bfd
Roman Serediuk <box4rom@gmail.com>
2023-08-30 16:18:09 +0300

    feat: create ResearchOperationsPage

---
+++ src/pages/ResearchOperationsPage.jsx
@@ -0,0 +1,12 @@
+import ResearchForm from "../components/Research/ResearchForm";
+
+const ResearchOperationsPage = () => {
+  return (
+    <section className="py-4">
+      <h4>ResearchOperationsPage</h4>
+      <ResearchForm />
+    </section>
+  );
+};
+
+export default ResearchOperationsPage;
