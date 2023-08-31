25aedaa027dbb5609ca4961fd6d5cb8babe83e03
Roman Serediuk <box4rom@gmail.com>
2023-08-31 14:08:35 +0300

    fix: array validation

--- src/assets/utils/validationSchema.js
+++ src/assets/utils/validationSchema.js
@@ -3,31 +3,42 @@ import * as yup from "yup";
 // TODO: validate array items, attachments
 
 const newsSchema = yup.object().shape({
-  title: yup.string().min(5, "Too short").required("Required"),
+  title: yup.string().min(5, "Too short").required("Title is required"),
   body: yup.string().min(10, "Too short"),
   date: yup.string().required("Required"),
-  link: yup.string(),
+  links: yup.array().of(yup.string().min(5, "Too short")),
 });
 
 const colleagueSchema = yup.object().shape({
-  name: yup.string().min(5, "Too short").required("Required"),
+  name: yup.string().min(5, "Too short").required("Name is required"),
   position: yup.string().min(5, "Too short").required("Required"),
   phone: yup.string(),
   email: yup.string(),
 });
 
 const publicationSchema = yup.object().shape({
-  title: yup.string().min(5, "Too short").required("Required"),
+  title: yup.string().min(5, "Too short").required("Title is required"),
   source: yup.string().min(5, "Too short").required("Required"),
   source_url: yup.string().min(5, "Too short").required("Required"),
-  // authors: yup.string().min(5, "Too short").required("Required"),
+  authors: yup
+    .array()
+    .of(yup.string().min(5, "Too short").required("Required")),
 });
 
 const researchSchema = yup.object().shape({
-  title: yup.string().required("Required"),
-  description: yup.string().required("Required"),
-  // source: yup.string().required("Required"),
-  // source_url: yup.string().required("Required"),
+  title: yup.string().min(5, "Too short").required("Title is required"),
+  sourceList: yup.array().of(
+    yup.object().shape({
+      source: yup.string().min(5, "Too short").required("Required"),
+      source_url: yup.string().min(5, "Too short").required("Required"),
+    })
+  ),
+  illustrationList: yup.array().of(
+    yup.object().shape({
+      description: yup.string().min(5, "Too short").required("Required"),
+      schema: yup.mixed().required("File is required"),
+    })
+  ),
 });
 
 export const validation = {
