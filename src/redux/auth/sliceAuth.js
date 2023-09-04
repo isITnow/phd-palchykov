6c9a25ceb07b58c14423bfc1ee8fbec5e4f07489
Roman Serediuk <box4rom@gmail.com>
2023-09-04 15:46:13 +0300

    refactor: implement builder.addCase

--- src/redux/auth/sliceAuth.js
+++ src/redux/auth/sliceAuth.js
@@ -11,28 +11,32 @@ const initStateAuth = {
 const authSlice = createSlice({
   name: "auth",
   initialState: initStateAuth,
-  extraReducers: {
-    [loginThunk.fulfilled]: (state, { payload }) => {
+  reducers: {},
+  extraReducers: (builder) => {
+    // SIGN IN USER
+    builder.addCase(loginThunk.fulfilled, (state, { payload }) => {
       state.user = payload.resource_owner;
       state.token = payload.token;
       state.refreshToken = payload.refresh_token;
       state.isLoggedIn = true;
-    },
-    [logoutThunk.fulfilled]: (state) => {
+    });
+    // LOG OUT USER
+    builder.addCase(logoutThunk.fulfilled, (state) => {
       state.user = null;
       state.token = null;
       state.refreshToken = null;
       state.isLoggedIn = false;
-    },
-    [refreshUserThunk.fulfilled]: (state, { payload }) => {
+    });
+    // REFRESH USER
+    builder.addCase(refreshUserThunk.fulfilled, (state, { payload }) => {
       state.user = payload;
       state.isLoggedIn = true;
-    },
-    [refreshUserThunk.rejected]: (state) => {
+    });
+    builder.addCase(refreshUserThunk.rejected, (state) => {
       state.user = { name: null, email: null };
       state.token = null;
       state.isLoggedIn = false;
-    },
+    });
   },
 });
 
