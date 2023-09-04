ee5d33645c5809ef26538a1603601910bf325e6a
Roman Serediuk <box4rom@gmail.com>
2023-09-04 11:08:52 +0300

    feat: create publicAPI and privateAPI; create set/unset token methods

--- src/services/http.js
+++ src/services/http.js
@@ -3,3 +3,20 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "http://localhost:3000/api/v1/",
 });
+
+export const privateAPI = axios.create({
+  baseURL: "http://localhost:3000/api/v1/",
+});
+
+export const publicAPI = axios.create({
+  baseURL: "http://localhost:3000/api/v1/",
+});
+
+export const token = {
+  set(token) {
+    privateAPI.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    privateAPI.defaults.headers.common.Authorization = null;
+  },
+};
