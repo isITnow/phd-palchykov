0e2e279a8ba4ae1871ba954a64ad5bc9944dfbd5
Roman Serediuk <box4rom@gmail.com>
2023-09-07 20:30:43 +0300

    create postsAPI; implement fetch, post, edit, delete methods

---
+++ src/services/postsAPI.js
@@ -0,0 +1,32 @@
+import { privateAPI, publicAPI } from "./http";
+
+const fetchPosts = async () => {
+  const data = await publicAPI.get("/posts");
+
+  return data;
+};
+
+const postPost = async (body) => {
+  const data = await privateAPI.post("/posts", body);
+
+  return data;
+};
+
+const editPost = async (id, body) => {
+  const data = await privateAPI.patch(`/post/${id}`, body);
+
+  return data;
+};
+
+const deletePost = async (id) => {
+  const data = await privateAPI.delete(`/post/${id}`);
+
+  return data;
+};
+
+export const newsAPI = {
+  fetchPosts,
+  postPost,
+  editPost,
+  deletePost,
+};
