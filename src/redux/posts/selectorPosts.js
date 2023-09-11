abaf4265e8c73c7cf31c64fac3f9ce0cbfbb3508
Roman Serediuk <box4rom@gmail.com>
2023-09-11 15:50:09 +0300

    feat: add error, status, onePost selectors

--- src/redux/posts/selectorPosts.js
+++ src/redux/posts/selectorPosts.js
@@ -1,1 +1,4 @@
-export const selectPosts = (state) => state.posts;
+export const selectPostsList = (state) => state.posts.postsList;
+export const selectOnePost = (state) => state.posts.onePost;
+export const selectError = (state) => state.posts.error;
+export const selectStatus = (state) => state.posts.status;
