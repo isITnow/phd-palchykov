189cb3847ab406ca4ec06c70b132939e9ce00fed
Roman Serediuk <box4rom@gmail.com>
2023-09-11 15:52:53 +0300

    feat: implement Post delete logic; implement Alert

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,19 +1,28 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 import { useDispatch, useSelector } from "react-redux";
 import {
   getOnePostThunk,
   removePostThunk,
-  updatePostThunk,
 } from "../redux/posts/operationsPosts";
-import { selectPosts } from "../redux/posts/selectorPosts";
+import {
+  selectOnePost,
+  selectError,
+  selectStatus,
+} from "../redux/posts/selectorPosts";
+
+import Alert from "../components/shared/Alert";
+import { useAlert } from "../assets/utils/useAlert";
 
 import Loader from "../components/shared/Loader";
 import Post from "../components/Posts/Post";
 import PostForm from "../components/Posts/PostForm";
 import Section from "../components/shared/Section";
+
+import setPageTitle from "../assets/utils/setPageTitle";
+import useSignInStatus from "../assets/utils/useSignInStatus";
 
 const fadeInOut = {
   initial: { opacity: 0 },
@@ -22,15 +31,52 @@ const fadeInOut = {
 };
 
 const PostPage = () => {
+  setPageTitle("Post");
+
   const { id } = useParams();
-  const [showForm, setShowForm] = useState(true);
+  const [showForm, setShowForm] = useState(false);
   const dispatch = useDispatch();
-  const { posts, status } = useSelector(selectPosts);
-  console.log("posts: ", posts);
+  const navigate = useNavigate();
+  const post = useSelector(selectOnePost);
+  const error = useSelector(selectError);
+  const status = useSelector(selectStatus);
+
+  const { alert, showAlert } = useAlert();
+  const isLoggedIn = useSignInStatus();
 
   useEffect(() => {
     dispatch(getOnePostThunk(id));
   }, [dispatch, id]);
+
+  useEffect(() => {
+    switch (status) {
+      case "rejected":
+        showAlert(`${error}. Please contact your administrator!`, "danger");
+        break;
+
+      case "fulfilled":
+        showAlert("Post updated successfully", "success");
+        setShowForm(false);
+        break;
+
+      case "removed":
+        setShowForm(false);
+        navigate("/posts");
+        break;
+      default:
+        break;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [status]);
+
+  const handleDelete = () => {
+    window.alert("Are you sure you want to delete post?");
+    dispatch(removePostThunk(id));
+  };
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
 
   if (status === "loading") {
     return <Loader />;
@@ -38,20 +84,56 @@ const PostPage = () => {
 
   return (
     <Section>
-      {posts.length && <Post post={posts[0]} single />}
+      <Alert state={alert} />
+      {post && <Post post={post} single />}
       <div className="my-3 text-end">
-        <div className="btn-group">
-          {true && (
-            <div
-              className="btn btn-primary"
-              onClick={() => setShowForm((prevState) => !prevState)}
-            >
-              edit
+        <button
+          type="button"
+          className="btn btn-outline-secondary"
+          onClick={handleGoBack}
+        >
+          go back
+        </button>
+        {isLoggedIn && (
+          <div className="btn-group ms-3">
+            {showForm ? (
+              <AnimatePresence>
+                <motion.button
+                  type="button"
+                  key="child"
+                  initial="initial"
+                  animate="animate"
+                  exit="exit"
+                  variants={fadeInOut}
+                  className="btn btn-outline-secondary"
+                  onClick={() => setShowForm(false)}
+                >
+                  cancel update
+                </motion.button>
+              </AnimatePresence>
+            ) : (
+              <AnimatePresence>
+                <motion.button
+                  type="button"
+                  key="child"
+                  initial="initial"
+                  animate="animate"
+                  exit="exit"
+                  variants={fadeInOut}
+                  className="btn btn-primary"
+                  onClick={() => setShowForm(true)}
+                >
+                  edit
+                </motion.button>
+              </AnimatePresence>
+            )}
+            <div className="btn btn-danger" onClick={handleDelete}>
+              delete
             </div>
-          )}
-          <div className="btn btn-danger">delete</div>
-        </div>
+          </div>
+        )}
       </div>
+
       <AnimatePresence>
         {showForm && (
           <motion.div
@@ -60,9 +142,8 @@ const PostPage = () => {
             animate="animate"
             exit="exit"
             variants={fadeInOut}
-            role="alert"
           >
-            <PostForm post={posts[0]} />
+            <PostForm post={post} />
           </motion.div>
         )}
       </AnimatePresence>
