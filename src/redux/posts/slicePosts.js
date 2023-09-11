338c91258eab655fa4b0c8313b63359678dbd97c
Roman Serediuk <box4rom@gmail.com>
2023-09-11 16:24:02 +0300

    feat: implement operationsComments

--- src/redux/posts/slicePosts.js
+++ src/redux/posts/slicePosts.js
@@ -7,6 +7,11 @@ import {
   updatePostThunk,
 } from "./operationsPosts";
 
+import {
+  addCommentThunk,
+  removeCommentThunk,
+} from "../comments/operationsComments";
+
 const initPosts = {
   postsList: [],
   onePost: null,
@@ -79,6 +84,32 @@ const postsSlice = createSlice({
       state.postsList = state.postsList.filter((post) => post.id !== payload);
     });
     builder.addCase(removePostThunk.rejected, setError);
+
+    //// COMMENTS ACTIONS /////
+
+    // CREATE COMMENT
+    builder.addCase(addCommentThunk.pending, (state) => {
+      state.status = "pending";
+      state.error = null;
+    });
+    builder.addCase(addCommentThunk.fulfilled, (state, { payload }) => {
+      state.status = "fulfilled";
+      state.onePost.comments.unshift(payload);
+    });
+    builder.addCase(addCommentThunk.rejected, setError);
+
+    // REMOVE COMMENT
+    builder.addCase(removeCommentThunk.pending, (state, { payload }) => {
+      state.status = "pending";
+      state.error = null;
+    });
+    builder.addCase(removeCommentThunk.fulfilled, (state, { payload }) => {
+      state.status = "removed";
+      state.onePost.comments = state.onePost.comments.filter(
+        (comment) => comment.id !== payload
+      );
+    });
+    builder.addCase(removeCommentThunk.rejected, setError);
   },
 });
 
