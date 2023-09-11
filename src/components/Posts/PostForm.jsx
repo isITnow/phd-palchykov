5dd57a39d18724b8ef87ffcca59e91154b7d16dc
Roman Serediuk <box4rom@gmail.com>
2023-09-11 13:51:46 +0300

    feat: add logic fo Post update

--- src/components/Posts/PostForm.jsx
+++ src/components/Posts/PostForm.jsx
@@ -1,12 +1,15 @@
 import { Form, Formik } from "formik";
 
 import { useDispatch } from "react-redux";
-import { addPostThunk } from "../../redux/posts/operationsPosts";
+import {
+  addPostThunk,
+  updatePostThunk,
+} from "../../redux/posts/operationsPosts";
 
 import CustomTextArea from "../FormComponents/CustomTextArea";
 import { validation } from "../../assets/utils/validationSchema";
 
-const PostForm = () => {
+const PostForm = ({ post }) => {
   const dispatch = useDispatch();
 
   const handleSubmit = async (values, actions) => {
@@ -15,27 +18,37 @@ const PostForm = () => {
     const formData = new FormData();
     formData.append("post[body]", body.trim());
 
-    dispatch(addPostThunk(formData));
-    actions.resetForm();
+    if (post) {
+      dispatch(updatePostThunk({ id: post.id, post: formData }));
+    } else {
+      dispatch(addPostThunk(formData));
+      actions.resetForm();
+    }
   };
 
   return (
     <Formik
       initialValues={{
-        body: "",
+        body: post ? post.body : "",
       }}
       validationSchema={validation.postSchema}
       onSubmit={handleSubmit}
     >
       {(props) => (
         <Form>
-          <CustomTextArea label="Post text" name="body" type="text" autoFocus />
+          <CustomTextArea
+            label="Post text"
+            name="body"
+            type="text"
+            rows="3"
+            autoFocus
+          />
           <button
             disabled={props.isSubmitting}
             type="submit"
             className="btn btn-primary"
           >
-            submit
+            {post ? "update" : "submit"}
           </button>
         </Form>
       )}
