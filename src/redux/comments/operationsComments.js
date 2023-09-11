75fca7ee12084d5b32eaeaaf5b3fdd9da95dc6a6
Roman Serediuk <box4rom@gmail.com>
2023-09-11 16:07:24 +0300

    create operationsComments.js, add addCommentThunk, removeCommentThunk

---
+++ src/redux/comments/operationsComments.js
@@ -0,0 +1,42 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { commentsAPI } from "../../services/commentsAPI";
+
+export const addCommentThunk = createAsyncThunk(
+  "comments/post",
+
+  async ({ post_id, comment }, { rejectWithValue }) => {
+    try {
+      const resp = await commentsAPI.postComment(post_id, comment);
+      console.log("resp: ", resp);
+
+      // if (resp.status !== 201) {
+      //   throw new Error("Error occurred! Please contact your administrator.");
+      // }
+
+      // return resp.data;
+    } catch (error) {
+      console.log("POST comment error: ", error);
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const removeCommentThunk = createAsyncThunk(
+  "comments/delete",
+
+  async ({ post_id, comment_id }, { dispatch, rejectWithValue }) => {
+    try {
+      const resp = await commentsAPI.deleteComment(post_id, comment_id);
+      console.log("resp: ", resp);
+
+      // if (resp.status !== 204) {
+      //   throw new Error("Error occurred! Please contact your administrator.");
+      // }
+
+      // return comment_id;
+    } catch (error) {
+      console.log("DELETE post error: ", error);
+      return rejectWithValue(error.message);
+    }
+  }
+);
