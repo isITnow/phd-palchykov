e1265950ed9c33d339010c7965667b18a096a524
Roman Serediuk <box4rom@gmail.com>
2023-09-12 11:36:24 +0300

    feat: animate publications on mount

--- src/components/Publications/PublicationsList.jsx
+++ src/components/Publications/PublicationsList.jsx
@@ -1,12 +1,28 @@
+import { motion } from "framer-motion";
 import Publication from "./Publication";
+
+const listItem = {
+  initial: { opacity: 0, y: -15 },
+  animate: { opacity: 1, y: 0 },
+  transition: { duration: 0.8 },
+  exit: { opacity: 0, y: -15 },
+};
 
 const PublicationsList = ({ publications }) => {
   return (
     <ul className="row row-cols-1 row-cols-md-2 g-2 mb-0">
       {publications.map((publication) => (
-        <li key={publication.id} className="col">
+        <motion.li
+          className="col"
+          key={publication.id}
+          initial="initial"
+          animate="animate"
+          transition="transition"
+          exit="exit"
+          variants={listItem}
+        >
           <Publication publication={publication} />
-        </li>
+        </motion.li>
       ))}
     </ul>
   );
