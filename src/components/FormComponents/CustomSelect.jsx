6f51cf46adda224d552353dd19fd51ae5cadaa38
Roman Serediuk <box4rom@gmail.com>
2023-09-14 11:48:08 +0300

    upd: remove outer div w-50

--- src/components/FormComponents/CustomSelect.jsx
+++ src/components/FormComponents/CustomSelect.jsx
@@ -3,7 +3,7 @@ import { useField } from "formik";
 const CustomSelect = ({ label, years, ...props }) => {
   const [field, meta] = useField(props.name);
   return (
-    <div className="w-50 mb-3">
+    <div className="mb-3">
       <label className="form-label px-3 text-secondary fw-bold">{label}</label>
       <select {...field} {...props} className="form-select">
         <option defaultValue={"Select"}>Select an option</option>
