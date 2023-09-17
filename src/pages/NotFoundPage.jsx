7967fff468c543674f0f11bcfd0f4e30510aea22
Roman Serediuk <box4rom@gmail.com>
2023-09-17 14:57:23 +0300

    upd: add Home link

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,12 +1,16 @@
+import { Link } from "react-router-dom";
 import React from "react";
 
 const NotFoundPage = () => {
   return (
-    <div className="mx-auto my-auto text-center">
+    <div className="mx-auto my-auto d-flex flex-column align-items-center">
       <h1 className="text-danger fw-bold" style={{ fontSize: "5rem" }}>
         404
       </h1>
       <span className="text-uppercase fw-bold fs-2">page not found</span>
+      <Link className="btn btn-outline-secondary mt-3" to="/">
+        Home
+      </Link>
     </div>
   );
 };
