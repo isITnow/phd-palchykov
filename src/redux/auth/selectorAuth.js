b2726b6210a0462f49a70ac3129c1410e348aaf6
Roman Serediuk <box4rom@gmail.com>
2023-09-17 13:31:54 +0300

    upd: add selectUserRole

--- src/redux/auth/selectorAuth.js
+++ src/redux/auth/selectorAuth.js
@@ -1,5 +1,6 @@
 export const selectIsLoggedIn = (state) => state.user.isLoggedIn;
-export const selectUserEmail = (state) => state.user.user.email;
-export const selectUserName = (state) => state.user.user.name;
+export const selectUserEmail = (state) => state.user.user?.email;
+export const selectUserName = (state) => state.user.user?.username;
+export const selectUserRole = (state) => state.user.user?.role;
 export const selectToken = (state) => state.user.token;
 export const selectError = (state) => state.user.error;
