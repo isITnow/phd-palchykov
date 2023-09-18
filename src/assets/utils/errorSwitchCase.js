fa87adfc4160190f2bf693b72e87e34f427e0ef3
Roman Serediuk <box4rom@gmail.com>
2023-09-18 11:38:31 +0300

    refactor: extract message with error.response.data.error || error.response.data

--- src/assets/utils/errorSwitchCase.js
+++ src/assets/utils/errorSwitchCase.js
@@ -1,12 +1,12 @@
 const errorSwitchCase = (error) => {
   switch (error.response.status) {
     case 401:
-      return error.response.data;
+      return error.response.data.error || error.response.data;
 
     case 400:
     case 404:
     case 422:
-      return error.response.data.error;
+      return error.response.data.error || error.response.data;
 
     default:
       return error.message;
