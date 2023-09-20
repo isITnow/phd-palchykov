de0be20c0ce5ff3c760246c0356e06c923cb57b1
Roman Serediuk <box4rom@gmail.com>
2023-09-20 18:17:39 +0300

    upd(Modal): remove size="lg"

--- src/components/Auth/AuthModal.jsx
+++ src/components/Auth/AuthModal.jsx
@@ -2,12 +2,7 @@ import Modal from "react-bootstrap/Modal";
 
 const AuthModal = (props) => {
   return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
+    <Modal {...props} aria-labelledby="contained-modal-title-vcenter" centered>
       <Modal.Header className="bg-warning bg-gradient" closeButton>
         <Modal.Title
           className="modal-title fw-bold fs-5 text-danger"
