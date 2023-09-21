86f31331944e322931911a6797e2e9709e7ccf9a
Roman Serediuk <box4rom@gmail.com>
2023-09-21 19:34:04 +0300

    upd: bg-danger

--- src/components/FormComponents/FormWarning.jsx
+++ src/components/FormComponents/FormWarning.jsx
@@ -1,7 +1,7 @@
 const FormWarning = ({ children }) => {
   return (
-    <div className="form-text alert px-3 py-1 bg-warning bg-gradient">
-      <span className="text-danger fw-bold">{children}</span>
+    <div className="form-text alert px-3 py-1 bg-danger bg-gradient">
+      <span className="text-light fw-bold">{children}</span>
     </div>
   );
 };
