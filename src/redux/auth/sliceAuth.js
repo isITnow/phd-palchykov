d6779c634b6052b0440efac85ec542286c84a888
Roman Serediuk <box4rom@gmail.com>
2023-09-25 17:33:25 +0300

    upd: add status

--- src/redux/auth/sliceAuth.js
+++ src/redux/auth/sliceAuth.js
@@ -2,10 +2,11 @@ import { createSlice } from "@reduxjs/toolkit";
 import { loginThunk, logoutThunk } from "./operationsAuth";
 
 const initStateAuth = {
-  user: null,
-  token: null,
-  isLoggedIn: false,
   error: null,
+  isLoggedIn: false,
+  status: null,
+  token: null,
+  user: null,
 };
 
 const authSlice = createSlice({
@@ -19,17 +20,20 @@ const authSlice = createSlice({
       state.token = null;
       state.isLoggedIn = false;
       state.error = null;
+      state.status = "pending";
     });
     builder.addCase(loginThunk.fulfilled, (state, { payload }) => {
       state.user = payload.user;
       state.token = payload.token;
       state.isLoggedIn = true;
+      state.status = "fulfilled";
     });
     builder.addCase(loginThunk.rejected, (state, { payload }) => {
       state.user = null;
       state.token = null;
       state.isLoggedIn = false;
       state.error = payload;
+      state.status = null;
     });
     // LOG OUT USER
     builder.addCase(logoutThunk.fulfilled, (state) => {
