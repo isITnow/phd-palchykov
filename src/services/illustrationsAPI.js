6665954632d3a7ad5ef1032562c6550c28ea59a9
Roman Serediuk <box4rom@gmail.com>
2023-10-10 22:26:24 +0300

    upd: create editIllustration; deleteIllustration methods

--- src/services/illustrationsAPI.js
+++ src/services/illustrationsAPI.js
@@ -9,6 +9,22 @@ const postIllustration = async (research_id, body) => {
   return data;
 };
 
+const editIllustration = async (research_id, id, body) => {
+  const data = await privateAPI.patch(`/researches/${research_id}/${id}`, body);
+
+  console.log("Illustration update: ", data);
+  // return data;
+};
+
+const deleteIllustration = async (research_id, id) => {
+  const data = await privateAPI.delete(`/researches/${research_id}/${id}`);
+
+  console.log("Illustration delete: ", data);
+  return data;
+};
+
 export const illustrationsAPI = {
   postIllustration,
+  editIllustration,
+  deleteIllustration,
 };
