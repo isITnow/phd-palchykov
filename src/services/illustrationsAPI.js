2eb7aaafae1126358423ca4a96fac6ce76f92f49
Roman Serediuk <104834083+isITnow@users.noreply.github.com>
2023-10-11 11:10:00 +0300

    Feature/researches and illustrations update (#12)
    * upd(NavLink): replace research by researches
    
    * upd: create editResearch method
    
    * upd: create editIllustration; deleteIllustration methods
    
    * feat: add update research logic; add update, remove illustration logic
    
    * feat: add updateResearchThunk
    
    * feat: add updateIllustrationThunk, removeIllustrationThunk
    
    * fix(Link): replace to={"/research/new"} by to={"/researches/new"}
    
    * upd: add edit Research btn
    
    * feat: create EditResearchPage
    
    * upd: add editResearchSchema, editIllustrationSchema
    
    * upd: add edit Illustration Link-btn
    
    * fix: implement editResearchSchema
    
    * upd: add comment "Remove thunk does not implemented in the app"
    
    * feat: create EditIllustrationPage

--- src/services/illustrationsAPI.js
+++ src/services/illustrationsAPI.js
@@ -9,6 +9,28 @@ const postIllustration = async (research_id, body) => {
   return data;
 };
 
+const editIllustration = async (research_id, id, body) => {
+  const data = await privateAPI.patch(
+    `/researches/${research_id}/illustrations/${id}`,
+    body
+  );
+
+  return data;
+};
+
+//* Remove api doesn't implemented in the app
+
+const deleteIllustration = async (research_id, id) => {
+  const data = await privateAPI.delete(
+    `/researches/${research_id}/illustrations/${id}`
+  );
+
+  console.log("Illustration delete: ", data);
+  return data;
+};
+
 export const illustrationsAPI = {
   postIllustration,
+  editIllustration,
+  deleteIllustration,
 };
