4ee73d668133b3825fd2c3dd286468587c578c2a
Roman Serediuk <box4rom@gmail.com>
2023-11-14 21:06:38 +0200

    switch BASE_URL

--- src/services/http.js
+++ src/services/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api/v1/";
-// const BASE_URL = "https://phd-palchykov-api.onrender.com/api/v1/";
+// const BASE_URL = "http://localhost:3000/api/v1/";
+const BASE_URL = "https://phd-palchykov-api.onrender.com/api/v1/";
 
 export const privateAPI = axios.create({
   baseURL: BASE_URL,
