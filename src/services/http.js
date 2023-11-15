575609565a0750946ab985fe5568956a765e77c1
Roman Serediuk <104834083+isITnow@users.noreply.github.com>
2023-11-15 11:25:15 +0200

    create env var REACT_APP_API_BASE_URL (#21)
    * create gallery API and PhotoAlbumForm
    
    * Feature/researches and illustrations update (#12)
    
    * upd(NavLink): replace research by researches
    
    * upd: create editResearch method
    
    * upd: create editIllustration; deleteIllustration methods
    
    * feat: add update research logic; add update, remove illustration logic
    
    * feat: add updateResearchThunk
    
    * feat: add updateIllustrationThunk, removeIllustrationThunk
    
    * fix(Link): replace to={"/research/new"} by to={"/researches/new"}
    
    * upd: add edit Research btn
    
    * feat: create EditResearchPage
    
    * upd: add editResearchSchema, editIllustrationSchema
    
    * upd: add edit Illustration Link-btn
    
    * fix: implement editResearchSchema
    
    * upd: add comment "Remove thunk does not implemented in the app"
    
    * feat: create EditIllustrationPage
    
    * upd: image wrapper div width "col-12 col-md-11 col-lg-9 "; source font size default
    
    * upd(add a new source input): replace push by insert
    
    * upd(sourceList): render multiple columns if list is more then 8 items long
    
    * fix(sourceListClass): add row-cols-1
    
    * upd(styles): add bs class to  render list of inputs as multi-column
    
    * upd(h4 style): replace "ms-3" by "ms-0  ms-lg-4 text-center text-lg-start"
    
    * refactor(setPageTitle): evoke setPageTitle in Layout useEffect
    
    * fix: add "nth-check": ">=2.0.1", "postcss": ">=8.4.31"
    
    * resolve conflict
    
    * fix: rename oparationsGallery to operationsGallery; fix import route
    
    * upd: add gallery to state
    
    * upd Spell config
    
    * upd: rename Gallery by PhotoAlbumsList
    
    * fix(selectPhotoAlbums): replace PhotoAlbums by photoAlbums
    
    * feat: fetch PhotoAlbums; render PhotoAlbumsList
    
    * upd: rename GalleryByTheme by PhotoAlbum
    
    * upd: rename GalleryByThemePage by PhotoAlbumPage
    
    * upd: add link to photo album by id
    
    * refactor(motion): create/implement motionOptions.js util
    
    * feat: fetch photo album by id; render album
    
    * feat: create PhotoAlbumOperationsPage.jsx; add create/edit photo album routes
    
    * upd: remove PhotoAlbumForm
    
    * upd: add photoAlbumSchema
    
    * feat: implement photo album update/delete
    
    * switch BASE_URL
    
    * update dependecies
    
    * refactor: create env var REACT_APP_API_BASE_URL

--- src/services/http.js
+++ src/services/http.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const BASE_URL = "http://localhost:3000/api/v1/";
-const BASE_URL = "https://phd-palchykov-api.onrender.com/api/v1/";
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const privateAPI = axios.create({
   baseURL: BASE_URL,
