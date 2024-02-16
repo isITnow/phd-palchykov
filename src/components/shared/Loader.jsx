b7eeb9019722cd9b5eb9745709bad0cc11b28dc4
Roman Serediuk <box4rom@gmail.com>
2024-02-16 18:17:31 +0200

    upd: add h3 "Waiting for server response. Thank you for your patience."

--- src/components/shared/Loader.jsx
+++ src/components/shared/Loader.jsx
@@ -1,14 +1,36 @@
+import { useEffect, useState } from "react";
+
 import { FallingLines } from "react-loader-spinner";
 
 const Loader = () => {
+  const [isWaitingForResponse, setIsWaitingForResponse] = useState(false);
+
+  useEffect(() => {
+    let timer;
+
+    timer = setTimeout(() => {
+      setIsWaitingForResponse(true);
+    }, 15000);
+
+    return () => {
+      clearTimeout(timer);
+      setIsWaitingForResponse(false);
+    };
+  }, []);
+
   return (
-    <div className="d-flex justify-content-center align-items-center">
+    <div className="d-flex flex-column justify-content-center align-items-center">
       <FallingLines
         color="#0275d8"
         width="9rem"
         visible={true}
         ariaLabel="falling-lines-loading"
       />
+      {isWaitingForResponse && (
+        <h3 className="text-center text-muted">
+          Waiting for server response. Thank you for your patience.
+        </h3>
+      )}
     </div>
   );
 };
