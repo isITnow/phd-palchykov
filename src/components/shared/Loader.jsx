fc9e0fcfa63c4d2cd495d3ffab79b97fb863d547
Roman Serediuk <box4rom@gmail.com>
2024-02-19 17:21:18 +0200

    upd: animate "Waiting for server response" notification

--- src/components/shared/Loader.jsx
+++ src/components/shared/Loader.jsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react";
-
+import { motion, AnimatePresence } from "framer-motion";
 import { FallingLines } from "react-loader-spinner";
+
+const fadeIn = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+};
 
 const Loader = () => {
   const [isWaitingForResponse, setIsWaitingForResponse] = useState(false);
@@ -27,9 +32,17 @@ const Loader = () => {
         ariaLabel="falling-lines-loading"
       />
       {isWaitingForResponse && (
-        <h3 className="text-center text-muted">
-          Waiting for server response. Thank you for your patience.
-        </h3>
+        <AnimatePresence>
+          <motion.h3
+            key="child"
+            initial="initial"
+            animate="animate"
+            variants={fadeIn}
+            className="text-center text-muted"
+          >
+            Waiting for server response. Thank you for your patience.
+          </motion.h3>
+        </AnimatePresence>
       )}
     </div>
   );
