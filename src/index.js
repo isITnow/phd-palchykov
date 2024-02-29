13e90ddc1d289ea39763cc87f2506714c4b54c32
Roman Serediuk <box4rom@gmail.com>
2024-02-29 09:25:12 +0200

    upd: add StrictMode

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 
 import { Provider } from "react-redux";
-import { store, persister } from "./redux/store";
+import { persister, store } from "./redux/store";
 
 import App from "./App";
 import Loader from "./components/shared/Loader";
 
-import "./index.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <PersistGate loading={<Loader />} persistor={persister}>
       <BrowserRouter>
-        <App />
+        <StrictMode>
+          <App />
+        </StrictMode>
       </BrowserRouter>
     </PersistGate>
   </Provider>
