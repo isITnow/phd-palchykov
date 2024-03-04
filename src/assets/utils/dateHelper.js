f33fc0a760104cdff286c879031f27ad98421a66
Roman Serediuk <box4rom@gmail.com>
2024-03-04 10:17:54 +0200

    remove comment line

--- src/assets/utils/dateHelper.js
+++ src/assets/utils/dateHelper.js
@@ -1,14 +1,12 @@
 import moment from "moment";
 
+export const currentDate = () => {
+  return moment().format("MMMM D, YYYY");
+};
+
 export const formateDate = (entity) => {
   const postedDate = moment(entity.created_at).fromNow();
   const editedDate = moment(entity.updated_at).fromNow();
   const isEdited = entity.updated_at > entity.created_at;
   return { postedDate, editedDate, isEdited };
 };
-
-export const currentDate = () => {
-  return moment().format("MMMM D, YYYY");
-};
-
-// export default formateDate;
