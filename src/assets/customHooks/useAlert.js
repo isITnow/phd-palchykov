8b0073d153872526d55347b04c603e07cdf76c1b
Roman Serediuk <box4rom@gmail.com>
2024-03-07 10:50:55 +0200

    fix(infinite re-render): implement useCallback

--- src/assets/customHooks/useAlert.js
+++ src/assets/customHooks/useAlert.js
@@ -1,15 +1,19 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 const initState = { text: "", type: "", isVisible: false };
 
 export const useAlert = () => {
   const [alert, setAlert] = useState(initState);
-  const showAlert = (text, type) => {
-    setAlert({ text, type, isVisible: true });
-    setTimeout(() => {
-      setAlert(initState);
-    }, 5000);
-  };
+
+  const showAlert = useCallback(
+    (text, type) => {
+      setAlert({ text, type, isVisible: true });
+      setTimeout(() => {
+        setAlert(initState);
+      }, 5000);
+    },
+    [setAlert]
+  );
 
   return { alert, showAlert };
 };
