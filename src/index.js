acc943c362b35ed6e728611661aa26f8ef49d832
Roman Serediuk <box4rom@gmail.com>
2025-02-10 11:56:48 +0200

    refactor: palce `queryClient` in separate file; add default queries opts

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
-import React, { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { QueryClientProvider } from '@tanstack/react-query';
 
-import { Provider } from "react-redux";
-import { persister, store } from "./redux/store";
+import { Provider } from 'react-redux';
+import { persister, store } from './redux/store';
+import queryClient from './queryClient.js';
 
-import App from "./App";
-import Loader from "./components/shared/Loader";
+import App from './App';
+import Loader from './components/shared/Loader';
 
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-import "react-toastify/dist/ReactToastify.css";
-import "./index.css";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'react-toastify/dist/ReactToastify.css';
+import './index.css';
 
-const queryClient = new QueryClient();
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <PersistGate loading={<Loader />} persistor={persister}>
