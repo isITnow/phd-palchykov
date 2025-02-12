4e11eb87f2d5d3aaaa4aa0d6f6a638be6585a7ac
Roman Serediuk <box4rom@gmail.com>
2025-02-12 11:28:20 +0200

    update: set as sync fn

--- src/assets/utils/confirmationDialog.js
+++ src/assets/utils/confirmationDialog.js
@@ -1,10 +1,6 @@
-const confirmationDialog = async (callback, message = 'Are you sure?') => {
+const confirmationDialog = (callback, message = 'Are you sure?') => {
   if (window.confirm(message) === true) {
-    try {
-      await callback();
-    } catch (error) {
-      console.error('Confirmation callback:', error);
-    }
+    callback();
   }
 };
 
