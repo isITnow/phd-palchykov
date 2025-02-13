b13bc3e4c11b08e12e990388f25bc71effe198a5
Roman Serediuk <box4rom@gmail.com>
2025-02-13 20:14:23 +0200

    update: wrap functions with the useCallback

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,28 +1,34 @@
-const useLocalStorage = (key) => {
-  const setItem = (value) => {
-    try {
-      window.localStorage.setItem(key, JSON.stringify(value));
-    } catch (error) {
-      console.log(error);
-    }
-  };
+import { useCallback } from 'react';
 
-  const getItem = () => {
+const useLocalStorage = (key) => {
+  const setItem = useCallback(
+    (value) => {
+      try {
+        window.localStorage.setItem(key, JSON.stringify(value));
+      } catch (error) {
+        console.error('Error setting localStorage:', error);
+      }
+    },
+    [key]
+  );
+
+  const getItem = useCallback(() => {
     try {
       const item = window.localStorage.getItem(key);
       return item ? JSON.parse(item) : undefined;
     } catch (error) {
-      console.log(error);
+      console.error('Error getting localStorage:', error);
+      return undefined;
     }
-  };
+  }, [key]);
 
-  const removeItem = () => {
+  const removeItem = useCallback(() => {
     try {
       window.localStorage.removeItem(key);
     } catch (error) {
-      console.log(error);
+      console.error('Error removing localStorage:', error);
     }
-  };
+  }, [key]);
 
   return { setItem, getItem, removeItem };
 };
