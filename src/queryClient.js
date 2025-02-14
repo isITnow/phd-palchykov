5bb8f1fbadef796b2244f42671423afe1ee4034c
Roman Serediuk <box4rom@gmail.com>
2025-02-14 10:56:22 +0200

    update(defaultOptions): encrease staleTime, cacheTime, gcTime

--- src/queryClient.js
+++ src/queryClient.js
@@ -1,25 +1,28 @@
 import { QueryClient } from '@tanstack/react-query';
 
 export const queryKeys = {
-    COLLEAGUES: ['colleagues'],
-    NEWS: ['news'],
-    PERIODS: ['periods'],
-    POSTS: ['posts'],
-    POST: (id) => ['post', id],
-    PUBLICATIONS: (id) => ['publications', id],
-    RESEARCHES: ['researches'],
-    PHOTO_ALBUMS: ['photoAlbums'],
-    PHOTO_ALBUM: (id) => ['photoAlbum', id],
+  COLLEAGUES: ['colleagues'],
+  NEWS: ['news'],
+  PERIODS: ['periods'],
+  POSTS: ['posts'],
+  POST: (id) => ['post', id],
+  PUBLICATIONS: (id) => ['publications', id],
+  RESEARCHES: ['researches'],
+  PHOTO_ALBUMS: ['photoAlbums'],
+  PHOTO_ALBUM: (id) => ['photoAlbum', id],
 };
 
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 24 * 60 * 60 * 1000, // 24 hours
-            gcTime: 24 * 60 * 60 * 1000, // 24 hours
-            refetchOnWindowFocus: false,
-        },
+  defaultOptions: {
+    queries: {
+      staleTime: 24 * 60 * 60 * 1000, // 24 години
+      cacheTime: 48 * 60 * 60 * 1000, // 48 годин
+      gcTime: 24 * 60 * 60 * 1000, // 24 години
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchInterval: false,
     },
+  },
 });
 
 export default queryClient;
