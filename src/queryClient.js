9eacc67b4d249b904f2d028f449607af99908682
Roman Serediuk <box4rom@gmail.com>
2025-02-14 10:47:39 +0200

    update: add photo album keys

--- src/queryClient.js
+++ src/queryClient.js
@@ -1,23 +1,25 @@
 import { QueryClient } from '@tanstack/react-query';
 
 export const queryKeys = {
-  COLLEAGUES: ['colleagues'],
-  NEWS: ['news'],
-  PERIODS: ['periods'],
-  POSTS: ['posts'],
-  POST: (id) => ['post', id],
-  PUBLICATIONS: (id) => ['publications', id],
-  RESEARCHES: ['researches'],
+    COLLEAGUES: ['colleagues'],
+    NEWS: ['news'],
+    PERIODS: ['periods'],
+    POSTS: ['posts'],
+    POST: (id) => ['post', id],
+    PUBLICATIONS: (id) => ['publications', id],
+    RESEARCHES: ['researches'],
+    PHOTO_ALBUMS: ['photoAlbums'],
+    PHOTO_ALBUM: (id) => ['photoAlbum', id],
 };
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 24 * 60 * 60 * 1000, // 24 hours
-      gcTime: 24 * 60 * 60 * 1000, // 24 hours
-      refetchOnWindowFocus: false,
+    defaultOptions: {
+        queries: {
+            staleTime: 24 * 60 * 60 * 1000, // 24 hours
+            gcTime: 24 * 60 * 60 * 1000, // 24 hours
+            refetchOnWindowFocus: false,
+        },
     },
-  },
 });
 
 export default queryClient;
