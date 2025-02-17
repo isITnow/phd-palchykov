84355c1dd3658f13508a1c79e13c1acca7c801c3
Roman Serediuk <box4rom@gmail.com>
2025-02-17 11:21:23 +0200

    fix: add vercel config to run SPA

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  build: { outDir: "dist" },
   plugins: [react()],
   resolve: {
     alias: {
       "@": "/src",
     },
   },
-  server: {
-    port: 3001,
-  },
+  server: { port: 3001 },
+  preview: { port: 3001 },
+  base: "/",
 });
