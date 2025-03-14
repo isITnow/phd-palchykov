23a6858cb2b6c06401e068a261261cdf587ab0e6
Roman Serediuk <box4rom@gmail.com>
2025-03-14 10:25:36 +0200

    chore: update Vite configuration to exclude `yup` from the build process

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  build: { outDir: "dist" },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      external: ["yup"],
+    }
+  },
   plugins: [react()],
   resolve: {
     alias: {
